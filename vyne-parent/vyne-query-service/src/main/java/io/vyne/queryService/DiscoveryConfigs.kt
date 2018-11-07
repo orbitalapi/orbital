cda6b505ee826fe39f2b224b1a020a4e2e486d23
Marty Pitt <martypitt@gmail.com>
2018-11-07 11:37:01 +0000

    allow additional options in docker image add logging to be clear when running embedded discovery vs not.

--- vyne-parent/vyne-query-service/src/main/java/io/vyne/queryService/DiscoveryConfigs.kt
+++ vyne-parent/vyne-query-service/src/main/java/io/vyne/queryService/DiscoveryConfigs.kt
@@ -1,17 +1,35 @@
 package io.vyne.queryService
 
+import io.vyne.utils.log
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.context.annotation.PropertySource
 
 @Profile("embedded-discovery")
 @EnableEurekaServer
 @Configuration
-class EmbeddedDiscoveryServerConfig
+class EmbeddedDiscoveryServerConfig {
+   @Value("\${eureka.dashboard.path}")
+   lateinit var eurekaDashboardPath: String
+
+   @Value("\${server.port}")
+   lateinit var serverPort: String
+
+   @Autowired
+   fun logInfo() {
+      log().info("An embedded Eureka instance is running.  Dashboard is available at  http://localhost:$serverPort/$eurekaDashboardPath")
+   }
+}
 
 @Profile("!embedded-discovery")
 @EnableEurekaClient
 @Configuration
-class ExternalDiscoveryServerConfig
+class ExternalDiscoveryServerConfig {
+   @Autowired
+   fun logInfo() {
+      log().info("Using external service discovery")
+   }
+}
