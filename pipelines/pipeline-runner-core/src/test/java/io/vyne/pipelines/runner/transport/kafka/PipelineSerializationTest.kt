207cefd8de4d2b9ea73ef22b211d024aca9763c7
psaulue-notional <paul.saulue-laborde@vyne.co>
2020-05-13 08:42:09 +0100

    Refactor with new Deserialisation service

--- pipelines/pipeline-runner-core/src/test/java/io/vyne/pipelines/runner/transport/kafka/PipelineSerializationTest.kt
+++ pipelines/pipeline-runner-core/src/test/java/io/vyne/pipelines/runner/transport/kafka/PipelineSerializationTest.kt
@@ -1,71 +1,138 @@
 package io.vyne.pipelines.runner.transport.kafka
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.winterbe.expekt.should
 import io.vyne.VersionedTypeReference
 import io.vyne.pipelines.Pipeline
 import io.vyne.pipelines.PipelineChannel
-import io.vyne.query.VyneJacksonModule
+import io.vyne.pipelines.runner.transport.PipelineJacksonModule
 import io.vyne.schemas.fqn
 import io.vyne.utils.log
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
-import org.junit.Ignore
+import org.junit.Before
 import org.junit.Test
 import java.util.*
 
-class PipelineSerializationTest() {
+class PipelineSerializationTest {
 
    fun producerProps(): Map<String, Any> {
       val props: MutableMap<String, Any> = HashMap<String, Any>()
       props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
-      props[ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG] = 3000
       props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-      props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
       return props
    }
 
    protected fun consumerProps(groupId: String): Map<String, Any> {
       val props: MutableMap<String, Any> = HashMap<String, Any>()
-      props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] =  "127.0.0.1:9092"
+      props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
       props[ConsumerConfig.GROUP_ID_CONFIG] = groupId
-      props[ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG] = 3000
-      props[ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG] = 3000
-      props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.canonicalName
-      props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.canonicalName
-      props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
       return props
    }
 
+   private lateinit var objectMapper: ObjectMapper
+
+   @Before
+   fun setup() {
+      objectMapper = jacksonObjectMapper().registerModule(PipelineJacksonModule())
+   }
+
    @Test
-   @Ignore
+   fun canDeserializePipeline() {
+      var pipelineJson = """
+            {
+              "name" : "test-pipeline",
+              "input" : {
+                "type" : "PersonLoggedOnEvent",
+                "transport" : {
+                  "topic" : "pipeline-input",
+                  "targetType" : "PersonLoggedOnEvent",
+                  "props" : {
+                    "group.id" : "vyne-pipeline-group",
+                    "bootstrap.servers" : "127.0.0.1:9092"
+                  },
+                  "direction" : "INPUT",
+                  "type" : "kafka"
+                }
+              },
+              "output" : {
+                "type" : "UserEvent",
+                "transport" : {
+                  "targetType" : "UserEvent",
+                  "direction" : "OUTPUT",
+                  "type" : "cask",
+                  "props" : {
+                  }
+               }
+              },
+              "id" : "test-pipeline@196343942"
+            }
+      """.trimIndent()
+
+      val pipeline = objectMapper.readValue(pipelineJson, Pipeline::class.java)
+
+      pipeline.id.should.equal("test-pipeline@196343942")
+      pipeline.name.should.equal("test-pipeline")
+      pipeline.input.transport.type.should.equal("kafka")
+      pipeline.output.transport.type.should.equal("cask")
+
+   }
+
+   @Test
    fun canSerializePipeline() {
-      val inputTopicName = "pipeline-input"
-      val outputTopicName = "pipeline-output"
+
       val pipeline = Pipeline(
-         input = PipelineChannel(
+         "test-pipeline",
+         PipelineChannel(
             VersionedTypeReference("PersonLoggedOnEvent".fqn()),
-            KafkaTransportInputSpec(
-               topic = inputTopicName,
-               targetType = VersionedTypeReference("PersonLoggedOnEvent".fqn()),
-               props = consumerProps("vyne-pipeline-group")
+            KafkaTransportInputSpec(topic = "pipeline-input", targetType = VersionedTypeReference("PersonLoggedOnEvent".fqn()), props = consumerProps("vyne-pipeline-group")
             )
          ),
-         output = PipelineChannel(
+         PipelineChannel(
             VersionedTypeReference("UserEvent".fqn()),
-            KafkaTransportOutputSpec(
-               topic = outputTopicName,
-               targetType = VersionedTypeReference("UserEvent".fqn()),
-               props = producerProps()
+            KafkaTransportOutputSpec(topic = "pipeline-output", targetType = VersionedTypeReference("UserEvent".fqn()), props = producerProps()
             )
-         ),
-         name = "test-pipeline"
+         )
       )
 
-      val json = jacksonObjectMapper()
-         .registerModule(VyneJacksonModule())
-         .writerWithDefaultPrettyPrinter().writeValueAsString(pipeline)
+      val json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(pipeline)
       log().info("PipelineJson: \n$json")
+
+      var pipelineJson = """
+            \{
+              "name" : "test-pipeline",
+              "input" : \{
+                "type" : "PersonLoggedOnEvent",
+                "transport" : \{
+                  "topic" : "pipeline-input",
+                  "targetType" : "PersonLoggedOnEvent",
+                  "props" : \{
+                    "group.id" : "vyne-pipeline-group",
+                    "bootstrap.servers" : "127.0.0.1:9092"
+                  \},
+                  "direction" : "INPUT",
+                  "type" : "kafka"
+                \}
+              \},
+              "output" : \{
+                "type" : "UserEvent",
+                "transport" : \{
+                  "topic" : "pipeline-output",
+                  "props" : \{
+                    "bootstrap.servers" : "127.0.0.1:9092",
+                    "key.serializer" : "org.apache.kafka.common.serialization.StringSerializer"
+                  \},
+                  "targetType" : "UserEvent",
+                  "direction" : "OUTPUT",
+                  "type" : "kafka"
+                \}
+              \},
+              "id" : "test-pipeline@[0-9]{10}"
+            \}
+      """.trimIndent()
+
+      json.should.match(Regex(pipelineJson))
    }
 }
