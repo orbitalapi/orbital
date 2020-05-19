3ac52eb29ef0f9faaad6ed0cadd4f90fba4292da
psaulue-notional <paul.saulue-laborde@vyne.co>
2020-05-19 09:40:27 +0100

    Register metadata

--- pipelines/pipeline-runner-core/src/main/java/io/vyne/pipelines/runner/jobs/RunnerJobController.kt
+++ pipelines/pipeline-runner-core/src/main/java/io/vyne/pipelines/runner/jobs/RunnerJobController.kt
@@ -1,5 +1,6 @@
 package io.vyne.pipelines.runner.jobs
 
+import com.netflix.appinfo.ApplicationInfoManager
 import io.vyne.pipelines.Pipeline
 import io.vyne.pipelines.runner.PipelineBuilder
 import io.vyne.pipelines.runner.PipelineInstanceReference
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class RunnerJobController(private val pipelineBuilder: PipelineBuilder) : PipelineRunnerApi {
+class RunnerJobController(private val pipelineBuilder: PipelineBuilder, val appInfoManager: ApplicationInfoManager) : PipelineRunnerApi {
+
+
+
    override fun submitPipeline(@RequestBody pipeline: Pipeline): PipelineInstanceReference {
       val instance = pipelineBuilder.build(pipeline)
+
+      appInfoManager.registerAppMetadata(mapOf(pipeline.input.transport.type to pipeline.output.transport.type))
       return instance
    }
 
