6ca7d426a6d22c5ef0563d97e5b71e04448d5645
Serhat Tuncay <serhat.tuncay@vyne.co>
2020-05-25 12:23:50 +0000

    Creating Cask Query Service Temporal Operators.

---
+++ cask/src/main/java/io/vyne/cask/query/generators/TemporalFieldUtils.kt
@@ -0,0 +1,52 @@
+package io.vyne.cask.query.generators
+
+import lang.taxi.Operator
+import lang.taxi.services.Parameter
+import lang.taxi.services.operations.constraints.PropertyToParameterConstraint
+import lang.taxi.services.operations.constraints.PropertyTypeIdentifier
+import lang.taxi.services.operations.constraints.RelativeValueExpression
+import lang.taxi.types.Annotation
+import lang.taxi.types.AttributePath
+import lang.taxi.types.CompilationUnit
+import lang.taxi.types.Field
+import lang.taxi.types.PrimitiveType
+import lang.taxi.types.Type
+
+object TemporalFieldUtils {
+   fun validate(field: Field): PrimitiveType? {
+      if (!PrimitiveType.isAssignableToPrimitiveType(field.type)) {
+         return null
+      }
+
+      val primitive = PrimitiveType.getUnderlyingPrimitive(field.type)
+      if (primitive != PrimitiveType.INSTANT && primitive != PrimitiveType.LOCAL_DATE) {
+         return null
+      }
+
+      return primitive
+   }
+
+   fun annotationFor(field: Field, name: String): Annotation? {
+      return field
+         .annotations
+         .find { annotation -> annotation.name == name }
+   }
+
+   fun constraintFor(field: Field, op: Operator, path: String) = PropertyToParameterConstraint(
+      propertyIdentifier = PropertyTypeIdentifier(field.type.toQualifiedName()),
+      operator = op,
+      expectedValue = RelativeValueExpression(AttributePath.from(path)),
+      compilationUnits = listOf(CompilationUnit.unspecified())
+   )
+
+   fun parameterFor(type: Type, name: String) = Parameter(
+      annotations = listOf(),
+      type = type,
+      name = name,
+      constraints = listOf())
+
+   const val Start = "start"
+   const val End = "end"
+   const val After = "after"
+   const val Before = "before"
+}
