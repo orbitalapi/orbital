82a77df947575852046f3857de4ab5f00f419c6d
Marty Pitt <martypitt@gmail.com>
2020-05-26 14:51:31 +0100

    add vyneql grammar and compiler

--- vyne-core-types/src/main/java/io/vyne/schemas/QualifiedName.kt
+++ vyne-core-types/src/main/java/io/vyne/schemas/QualifiedName.kt
@@ -1,6 +1,7 @@
 package io.vyne.schemas
 
 import lang.taxi.types.PrimitiveType
+import lang.taxi.types.QualifiedNameParser
 import java.io.IOException
 import java.io.Serializable
 import java.io.StreamTokenizer
@@ -43,68 +44,14 @@ fun String.fqn(): QualifiedName {
    return when {
       OperationNames.isName(this) -> QualifiedName(this, emptyList())
       ParamNames.isParamName(this) -> QualifiedName("param/" + ParamNames.typeNameInParamName(this).fqn().parameterizedName)
-      else -> parse(this).toQualifiedName()
-   }
-
-}
-
-private data class GenericTypeName(val baseType: String, val params: List<GenericTypeName>) {
-   fun toQualifiedName(): QualifiedName {
-      return QualifiedName(this.baseType, this.params.map { it.toQualifiedName() })
-   }
-}
-
-private fun parse(s: String): GenericTypeName {
-   val expandedName = convertArrayShorthand(s)
-   val tokenizer = StreamTokenizer(StringReader(expandedName))
-   tokenizer.wordChars('_'.toInt(), '_'.toInt())
-   try {
-      return parse(tokenizer, listOf(StreamTokenizer.TT_EOF)) // Parse until the end
-   } catch (e: IOException) {
-      throw RuntimeException()
-   }
-
-}
-
-
-// Converts Foo[] to lang.taxi.Array<Foo>
-private fun convertArrayShorthand(name: String): String {
-   if (name.endsWith("[]")) {
-      val arrayType = name.removeSuffix("[]")
-      return PrimitiveType.ARRAY.qualifiedName + "<$arrayType>"
-   } else {
-      return name
-   }
-}
-
-private fun parse(tokenizer: StreamTokenizer, terminalCharacterCodes: List<Int>): GenericTypeName {
-   val BOF_MARKER = -4
-   val baseNameParts = mutableListOf<String>()
-   val params = mutableListOf<GenericTypeName>()
-   try {
-
-      while (!terminalCharacterCodes.contains(tokenizer.ttype)) {
-         when (tokenizer.ttype) {
-            BOF_MARKER -> {
-               tokenizer.nextToken() // Skip it
-            }
-            '<'.toInt() -> {
-               do {
-                  tokenizer.nextToken()  // Skip '<' or ','
-                  params.add(parse(tokenizer, terminalCharacterCodes = listOf('>'.toInt(), ','.toInt())))
-               } while (tokenizer.ttype == ','.toInt())
-               tokenizer.nextToken() // Skip past the closing >
-            }
-            else -> {
-               baseNameParts.add(tokenizer.sval)
-               tokenizer.nextToken()
-            }
-         }
+      else -> {
+         val taxiQualifiedName = QualifiedNameParser.parse(this)
+         QualifiedName(
+            taxiQualifiedName.fullyQualifiedName,
+            taxiQualifiedName.parameters.map { it.toVyneQualifiedName() }
+         )
       }
-   } catch (e:Exception) {
-      // TODO  : Bring logging in here...
-      throw e
    }
-   return GenericTypeName(baseNameParts.joinToString(""), params)
+
 }
 
---
+++ vyne-ql/vyne-ql-compiler/src/main/java/io/vyne/vyneql/compiler/ConstraintBuilder.kt
@@ -0,0 +1,70 @@
+package io.vyne.vyneql.compiler
+
+import arrow.core.Either
+import io.vyne.VyneQLParser
+import lang.taxi.*
+import lang.taxi.services.operations.constraints.Constraint
+import lang.taxi.services.operations.constraints.PropertyToParameterConstraintAst
+import lang.taxi.services.operations.constraints.PropertyToParameterConstraintProvider
+import lang.taxi.types.AttributePath
+import lang.taxi.types.Type
+import lang.taxi.utils.invertEitherList
+import org.antlr.v4.runtime.ParserRuleContext
+
+class ConstraintBuilder(schema:TaxiDocument, private val typeResolver: NamespaceQualifiedTypeResolver) {
+   private val propertyToParameterConstraintProvider = PropertyToParameterConstraintProvider()
+
+   fun build(parameterConstraintExpressionList: VyneQLParser.ParameterConstraintExpressionListContext,
+             type: Type
+   ): Either<List<CompilationError>, List<Constraint>> {
+      val constraints: Either<List<CompilationError>, List<Constraint>> = parameterConstraintExpressionList.parameterConstraintExpression().map { constraintExpression ->
+         val ast = parseToAst(constraintExpression.propertyToParameterConstraintExpression())
+         propertyToParameterConstraintProvider.build(ast, type, typeResolver, constraintExpression)
+      }.invertEitherList()
+      return constraints
+   }
+
+   /**
+    * We use an AST for parsing constraints as we need to support these in both
+    * Taxi and VyneQL.  However, the Antlr generation tree makes using the same code
+    * in both contexts very difficult, as it regenerates the classes in a different namespace.
+    * This means even though the classes are generated from the exact same source,
+    * we can't pass them into the constraint provider code.
+    * Therefore, we parse a simple AST, which makes reuse possible
+    */
+   @Suppress("DuplicatedCode")
+   private fun parseToAst(constraint: VyneQLParser.PropertyToParameterConstraintExpressionContext): PropertyToParameterConstraintAst {
+      val astLhs = constraint.propertyToParameterConstraintLhs().let { lhs ->
+         val typeOrFieldQualifiedName = lhs.qualifiedName().asDotJoinedPath()
+         val propertyFieldNameQualifier = lhs.propertyFieldNameQualifier()?.text
+         PropertyToParameterConstraintAst.AstLhs(propertyFieldNameQualifier, typeOrFieldQualifiedName)
+      }
+
+      val astRhs = constraint.propertyToParameterConstraintRhs().let { rhs ->
+         val literal = rhs.literal()?.value()
+         val attributePath = rhs.qualifiedName()?.asDotJoinedPath()?.let { AttributePath.from(it) }
+         PropertyToParameterConstraintAst.AstRhs(literal, attributePath)
+      }
+
+      val operator = constraint.comparisonOperator().text
+      return PropertyToParameterConstraintAst(astLhs, operator, astRhs)
+   }
+}
+
+private fun VyneQLParser.QualifiedNameContext.asDotJoinedPath(): String {
+   return this.Identifier().joinToString(".") { it.text }
+}
+
+private fun VyneQLParser.LiteralContext.value(): Any {
+   return when {
+      this.StringLiteral() != null -> {
+         // can be either ' or "
+         val firstChar = this.StringLiteral().text.toCharArray()[0]
+         this.StringLiteral().text.trim(firstChar)
+      }
+      this.IntegerLiteral() != null -> this.IntegerLiteral().text.toInt()
+      this.BooleanLiteral() != null -> this.BooleanLiteral().text!!.toBoolean()
+      else -> TODO()
+//      this.IntegerLiteral() != null -> this.IntegerLiteral()
+   }
+}
