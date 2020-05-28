6dd750cc4b43319bd4feceafccfbad189a1cb971
psaulue-notional <paul.saulue-laborde@vyne.co>
2020-05-28 15:54:01 +0100

    Reset cask changes

--- cask-api/src/test/java/io/vyne/cask/api/CaskIngestionResponseTest.kt
+++ cask-api/src/test/java/io/vyne/cask/api/CaskIngestionResponseTest.kt
@@ -6,13 +6,13 @@ import org.junit.Test
 
 class CaskIngestionResponseTest {
 
-   @Test
-   fun testSerializationAndDeserialization() {
-      val mapper = jacksonObjectMapper()
-      val response = CaskIngestionResponse.success("Successful ingestion")
-      val jsonMessage = """{"result":"SUCCESS","message":"Successful ingestion"}"""
+    @Test
+    fun testSerializationAndDeserialization() {
+        val mapper = jacksonObjectMapper()
+        val response = CaskIngestionResponse.success("Successful ingestion")
+        val jsonMessage = """{"result":"SUCCESS","message":"Successful ingestion"}"""
 
-      mapper.writeValueAsString(response).should.equal(jsonMessage)
-      mapper.readValue(jsonMessage, CaskIngestionResponse::class.java).should.equal(response)
-   }
+        mapper.writeValueAsString(response).should.equal(jsonMessage)
+        mapper.readValue(jsonMessage, CaskIngestionResponse::class.java).should.equal(response)
+    }
 }
