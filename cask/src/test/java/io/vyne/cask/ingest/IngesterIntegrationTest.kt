50bc3b24fb3ede828fad0eaf5bff0499072a65be
Serhat Tuncay <serhat@sigun.com.tr>
2020-06-05 11:05:54 +0300

    PR comments

--- cask/src/test/java/io/vyne/cask/ingest/IngesterIntegrationTest.kt
+++ cask/src/test/java/io/vyne/cask/ingest/IngesterIntegrationTest.kt
@@ -1,13 +1,18 @@
 package io.vyne.cask.ingest
 
+import arrow.core.getOrElse
 import com.google.common.io.Resources
+import com.opentable.db.postgres.embedded.EmbeddedPostgres
 import com.opentable.db.postgres.junit.EmbeddedPostgresRules
 import com.winterbe.expekt.should
+import io.vyne.cask.CaskService
 import io.vyne.cask.ddl.TableMetadata
 import io.vyne.cask.ddl.TypeDbWrapper
 import io.vyne.cask.format.csv.CoinbaseOrderSchema
 import io.vyne.cask.format.csv.CsvStreamSource
+import io.vyne.cask.websocket.CsvIngestionRequest
 import io.vyne.schemas.fqn
+import io.vyne.spring.LocalResourceSchemaProvider
 import io.vyne.utils.Benchmark
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.io.FileUtils
@@ -23,6 +28,7 @@ import org.springframework.jdbc.core.JdbcTemplate
 import reactor.core.publisher.Flux
 import java.io.File
 import java.io.InputStream
+import java.nio.file.Paths
 
 @Ignore
 class IngesterIntegrationTest {
@@ -31,9 +37,7 @@ class IngesterIntegrationTest {
    @JvmField
    val folder = TemporaryFolder()
 
-   @Rule
-   @JvmField
-   val pg = EmbeddedPostgresRules.singleInstance().customize { it.setPort(6661) }
+   lateinit var pg: EmbeddedPostgres
    // Atm there is no way to override dbname/username/pwd
 
    lateinit var jdbcTemplate: JdbcTemplate
@@ -41,17 +45,19 @@ class IngesterIntegrationTest {
 
    @Before
    fun setup() {
+      pg = EmbeddedPostgres.builder().setPort(6660).start()
       val dataSource = DataSourceBuilder.create()
          .url("jdbc:postgresql://localhost:6660/postgres")
          .username("postgres")
          .build()
       jdbcTemplate = JdbcTemplate(dataSource)
       jdbcTemplate.execute(TableMetadata.DROP_TABLE)
+
    }
 
    @After
    fun tearDown() {
-      ingester.destroy()
+      pg.close()
    }
 
    @Test
@@ -83,6 +89,25 @@ class IngesterIntegrationTest {
          ingester.destroy()
          FileUtils.cleanDirectory(folder.root);
       }
+   }
+
+   @Test
+   fun canIngestCsvToCask() {
+      val source = Resources.getResource("Coinbase_BTCUSD_single.csv").toURI()
+      val input: Flux<InputStream> = Flux.just(File(source).inputStream())
+      val schemaProvider = LocalResourceSchemaProvider(Paths.get(Resources.getResource("schemas/coinbase").toURI()))
+      val ingesterFactory = IngesterFactory(jdbcTemplate)
+      val caskService = CaskService(
+         schemaProvider,
+         ingesterFactory
+      )
+      val type = caskService.resolveType("OrderWindowSummaryCsv").getOrElse {
+         error("Type not found")
+      }
+      caskService.ingestRequest(
+         CsvIngestionRequest(CSVFormat.DEFAULT.withFirstRecordAsHeader(), type, emptySet()),
+         input
+      ).blockFirst()
    }
 
    @Test
