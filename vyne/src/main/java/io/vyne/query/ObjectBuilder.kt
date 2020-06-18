53fb84a0bc80fd9cf4bb2beaa5aea68899b6c0ea
psaulue-notional <paul.saulue-laborde@vyne.co>
2020-06-18 10:29:58 +0100

    Reverted debug log

--- vyne/src/main/java/io/vyne/query/ObjectBuilder.kt
+++ vyne/src/main/java/io/vyne/query/ObjectBuilder.kt
@@ -2,7 +2,6 @@ package io.vyne.query
 
 import io.vyne.models.*
 import io.vyne.schemas.*
-import io.vyne.utils.log
 import lang.taxi.types.ObjectType
 
 class ObjectBuilder(val queryEngine: QueryEngine, val context: QueryContext) {
@@ -84,7 +83,8 @@ class ObjectBuilder(val queryEngine: QueryEngine, val context: QueryContext) {
 
       }.toMap()
       if (missingAttributes.isNotEmpty()) {
-         log().debug("Couldn't build instance of ${targetType.fullyQualifiedName} as the following attributes weren't found: \n ${missingAttributes.map { (name, field) -> "$name : ${field.type.fullyQualifiedName}" }.joinToString("\n")}")
+         // Commenting out as printing 1000 entries for simple projection will create unwanted noise!
+         // log().warn("Couldn't build instance of ${targetType.fullyQualifiedName} as the following attributes weren't found: \n ${missingAttributes.map { (name, field) -> "$name : ${field.type.fullyQualifiedName}" }.joinToString("\n")}")
       }
       return TypedObject.fromAttributes(targetType, mappedValues, context.schema)
    }
