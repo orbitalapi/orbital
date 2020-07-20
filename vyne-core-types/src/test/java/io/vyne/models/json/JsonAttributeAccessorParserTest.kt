987d750abca15c5a4b494ee3eeec291e8b98a5fa
psaulue-notional <paul.saulue-laborde@vyne.co>
2020-07-20 17:24:16 +0100

    Empty enums in json treated as null

--- vyne-core-types/src/test/java/io/vyne/models/json/JsonAttributeAccessorParserTest.kt
+++ vyne-core-types/src/test/java/io/vyne/models/json/JsonAttributeAccessorParserTest.kt
@@ -2,13 +2,11 @@ package io.vyne.models.json
 
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.eq
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.*
 import com.winterbe.expekt.should
 import io.vyne.models.PrimitiveParser
 import io.vyne.models.Provided
+import io.vyne.schemas.Type
 import junit.framework.Assert.fail
 import lang.taxi.types.XpathAccessor
 import org.junit.Before
@@ -77,6 +75,36 @@ class JsonAttributeAccessorParserTest() {
       }catch(e: IllegalStateException) {
          e.message.should.be.equal("Could not find xpath /year in record")
       }
+
+   }
+
+   @Test
+   fun parseEnum() {
+      val accessor = XpathAccessor("/country")
+
+      var node = jacksonObjectMapper().readTree(""" {  "country": "France" } """) as ObjectNode
+
+      var enumMock = mock<Type>()
+      doReturn(true).whenever(enumMock).isEnum
+      parser.parseToType(enumMock, accessor, node, mock(), Provided)
+
+      verify(primitiveParser).parse(eq("France"), any(), eq(Provided))
+
+   }
+
+   @Test
+   fun parseEmptyEnum() {
+      val accessor = XpathAccessor("/country")
+
+      var node = jacksonObjectMapper().readTree(""" {  "country": "" } """) as ObjectNode
+
+      var enumMock = mock<Type>()
+      doReturn(true).whenever(enumMock).isEnum
+
+      val instance = parser.parseToType(enumMock, accessor, node, mock(), Provided)
+      instance.value.should.be.`null`
+
+      verify(primitiveParser, never()).parse(any(), any(), eq(Provided))
 
    }
 }
