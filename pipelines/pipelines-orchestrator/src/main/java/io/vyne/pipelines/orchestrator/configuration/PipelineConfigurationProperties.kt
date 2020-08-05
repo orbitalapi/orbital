55a467c73289fcee55f25c2bde5cf76bd15113cf
Serhat Tuncay <serhat.tuncay@vyne.co>
2020-08-05 12:37:18 +0000

    Pipeline definitions in yml format

--- pipelines/pipelines-orchestrator/src/main/java/io/vyne/pipelines/orchestrator/configuration/PipelineConfigurationProperties.kt
+++ pipelines/pipelines-orchestrator/src/main/java/io/vyne/pipelines/orchestrator/configuration/PipelineConfigurationProperties.kt
@@ -3,10 +3,12 @@ package io.vyne.pipelines.orchestrator.configuration
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.core.io.Resource
-import org.springframework.stereotype.Component
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "vyne.pipeline")
-data class PipelineConfigurationProperties(val definition: List<String> = listOf(), val definitions: FileBasedPipelineConfigurations? = null)
+data class PipelineConfigurationProperties(val definition: List<PipelineConfigurationProperty> = listOf(), val definitions: FileBasedPipelineConfigurations? = null)
 
 data class FileBasedPipelineConfigurations(val location: Resource)
+
+data class PipelineConfigurationProperty(val name: String, val input: Map<String, Any>, val output: Map<String, Any>)
+
