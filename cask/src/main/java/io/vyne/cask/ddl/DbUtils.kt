775df1094cdf202068b162a52201dcca14df1035
devrim.bostanci <devrim.bostanci@sigun.com.tr>
2020-08-20 18:33:08 +0300

    Large object, tests are failing

--- cask/src/main/java/io/vyne/cask/ddl/DbUtils.kt
+++ cask/src/main/java/io/vyne/cask/ddl/DbUtils.kt
@@ -1,7 +1,9 @@
 package io.vyne.cask.ddl
 
+import com.zaxxer.hikari.HikariDataSource
 import org.postgresql.PGConnection
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
@@ -9,34 +11,20 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 import org.springframework.stereotype.Component
 import java.sql.Connection
-import java.sql.DriverManager
 import javax.sql.DataSource
+
 
 fun JdbcTemplate.pgConnection() = this.dataSource.pgConnection()
 
 fun DataSource.pgConnection(): PGConnection {
     return this.connection.unwrap(PGConnection::class.java)
 }
-fun JdbcTemplate.getCustomConnection(): Connection? {
-   val conn = this.dataSource.connection
-   conn.autoCommit = false
-   return conn
+fun JdbcTemplate.enableAutoCommit() {
+   this.dataSource.connection.autoCommit = true
 }
-
-@Component
-class DbPlus {
-   @Autowired
-   private lateinit var env: Environment
-
-   @Bean
-   fun getCustomConnection(): Connection? {
-      val dataSource = DriverManagerDataSource()
-
-      dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName")!!)
-      dataSource.url = env.getProperty("spring.datasource.url")
-      dataSource.username = env.getProperty("spring.datasource.username")
-      dataSource.password = env.getProperty("spring.datasource.password")
-
-      return dataSource.connection
-   }
+fun JdbcTemplate.disableAutoCommit() {
+   this.dataSource.connection.autoCommit = false
+}
+fun JdbcTemplate.getConnection(): Connection? {
+   return this.dataSource?.connection
 }
