d7b8426a28a2b39d55f8cb4135d20d62a3ae420a
Serhat Tuncay <serhat.tuncay@vyne.co>
2020-08-25 12:10:12 +0300

    LENS-309

--- vyne-core-types/src/main/java/io/vyne/models/conditional/CalculatedFieldSetExpressionEvaluator.kt
+++ vyne-core-types/src/main/java/io/vyne/models/conditional/CalculatedFieldSetExpressionEvaluator.kt
@@ -2,11 +2,14 @@ package io.vyne.models.conditional
 
 import io.vyne.formulas.CalculatorRegistry
 import io.vyne.models.Calculated
+import io.vyne.models.DefinedInSchema
 import io.vyne.models.TypedInstance
+import io.vyne.models.TypedNull
 import io.vyne.models.TypedObjectFactory
 import io.vyne.schemas.AttributeName
 import io.vyne.schemas.Type
 import lang.taxi.types.CalculatedFieldSetExpression
+import lang.taxi.types.UnaryCalculatedFieldSetExpression
 
 class CalculatedFieldSetExpressionEvaluator(
    private val factory: TypedObjectFactory,
@@ -16,5 +19,19 @@ class CalculatedFieldSetExpressionEvaluator(
       val calculator = calculatorRegistry.getCalculator(readCondition.operator, operandValues.map { it.type }) ?: error("Invalid calculated field")
       val value = calculator.calculate(readCondition.operator, operandValues.map { it.value!! })
       return TypedInstance.from(targetType, value, factory.schema, source = Calculated)
+   }
+
+   fun evaluate(readCondition: UnaryCalculatedFieldSetExpression, attributeName: AttributeName?, targetType: Type): TypedInstance {
+      val operandTypedInstance = factory.getValue(readCondition.operand.fieldName)
+      val operandType = operandTypedInstance.type
+      val unaryCalculator = calculatorRegistry.getUnaryCalculator(readCondition.operator, operandType)
+         ?: error("Invalid unary calculated field $attributeName ${readCondition.operator}")
+      val opValue = operandTypedInstance.value
+      return if (opValue == null) {
+         TypedNull(operandType, source = DefinedInSchema)
+      } else {
+         val value = unaryCalculator.calculate(readCondition.operator, opValue, readCondition.literal)
+         TypedInstance.from(targetType, value, factory.schema, source = Calculated)
+      }
    }
 }
