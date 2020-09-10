2374b64837d0904846a1700679ba197baec2c822
mert <hmertalbayrak@yandex.com>
2020-09-10 12:15:22 +0300

    UI/Tooltips for graphs

--- vyne-query-service/src/main/web/src/app/inheritence-graph/inheritance-graph.module.ts
+++ vyne-query-service/src/main/web/src/app/inheritence-graph/inheritance-graph.module.ts
@@ -3,6 +3,7 @@ import {InheritanceGraphComponent} from './inheritance-graph.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
 import {NgxGraphModule} from '@swimlane/ngx-graph';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -10,6 +11,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserModule,
     CommonModule,
     NgxGraphModule,
+    NgxChartsModule,
     BrowserAnimationsModule
   ],
   exports: [InheritanceGraphComponent],
