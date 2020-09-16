20b96306dd59cd4ec7dad88b9e516c60558446f6
Marty Pitt <martypitt@gmail.com>
2020-09-16 12:39:18 +0100

    update to new stdlib in taxi 0.9.2

--- vyne-core-types/src/main/java/io/vyne/models/functions/stdlib/StdLib.kt
+++ vyne-core-types/src/main/java/io/vyne/models/functions/stdlib/StdLib.kt
@@ -7,29 +7,5 @@ object StdLib {
    val functions = listOf(
       Strings.functions
    ).flatten()
-
-   val taxi = VersionedSource("vyne.stdlib", "0.0", functions
-      .filterIsInstance<SelfDescribingFunction>()
-      .namespacedTaxi())
 }
 
-
-fun List<SelfDescribingFunction>.namespacedTaxi():String {
-   val result = groupBy { it.functionName.namespace }
-      .map { (namespace,functions) ->
-         val functionTaxi = functions.joinToString("\n") { it.taxiDeclaration }
-         """namespace $namespace {
-            |$functionTaxi
-            |}
-         """.trimMargin()
-      }.joinToString("\n")
-   return result
-
-}
-
-fun String.inNamespace(namespace:String):String {
-   return """namespace $namespace {
-      |$this
-      |}
-   """.trimMargin()
-}
