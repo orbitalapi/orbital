c6874b0d87b42a61fba563c7c500dbdb4499e94f
Marty Pitt <martypitt@gmail.com>
2020-09-18 21:07:03 +0100

    support calculations on varying number types by casting to bigdecimal

--- vyne-core-types/src/test/java/io/vyne/formulas/CalculatorTest.kt
+++ vyne-core-types/src/test/java/io/vyne/formulas/CalculatorTest.kt
@@ -7,6 +7,7 @@ import lang.taxi.types.Formula
 import lang.taxi.types.FormulaOperator
 import org.junit.Test
 import java.math.BigDecimal
+import kotlin.test.fail
 
 class CalculatorTest {
    data class TestScenario(val inputA:Any, val inputB:Any, val operator: FormulaOperator, val expected:Any) {
@@ -26,6 +27,13 @@ class CalculatorTest {
          TestScenario(1L, 2L, FormulaOperator.Add, 3L),
          TestScenario(1.5, 4.0, FormulaOperator.Add, 5.5),
          TestScenario(1.5, 4.0, FormulaOperator.Multiply, 6.0),
+
+         // When mixing types, you'll get a BigDecimal back
+         TestScenario(1.5, 4, FormulaOperator.Multiply, 6.0.toBigDecimal()),
+         TestScenario(2L, 4.0, FormulaOperator.Multiply, 8.0.toBigDecimal()),
+         TestScenario(4, 0.55003, FormulaOperator.Divide, 7.272330600149083.toBigDecimal()),
+
+
          TestScenario(BigDecimal.TEN, BigDecimal.valueOf(5), FormulaOperator.Multiply, BigDecimal.valueOf(50)),
          TestScenario(BigDecimal.TEN, BigDecimal.valueOf(5), FormulaOperator.Subtract, BigDecimal.valueOf(5)),
          TestScenario(BigDecimal.TEN, BigDecimal.valueOf(5), FormulaOperator.Subtract, BigDecimal.valueOf(5))
@@ -51,6 +59,11 @@ class CalculatorTest {
       val formula = mock<Formula> {
          on { operator }.doReturn(scenario.operator)
       }
-      calculator.calculate(formula.operator, scenario.values).should.equal(scenario.expected)
+      try {
+         calculator.calculate(formula.operator, scenario.values).should.equal(scenario.expected)
+      } catch (e: Throwable) {
+         fail(scenario.toString() + " - " + e.message)
+      }
+
    }
 }
