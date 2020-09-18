a720dbcd6ddebe97767a105ca72825cc40016bdd
Marty Pitt <martypitt@gmail.com>
2020-09-18 14:23:42 +0100

    added missing file

---
+++ vyne/src/main/java/io/vyne/query/build/FirstNotEmptyPredicate.kt
@@ -0,0 +1,29 @@
+package io.vyne.query.build
+
+import io.vyne.models.TypedInstance
+import io.vyne.models.TypedNull
+import io.vyne.query.TypedInstanceValidPredicate
+import io.vyne.schemas.Field
+
+/**
+ * Rejects a value if it is null, empty, or whitespace
+ */
+object FirstNotEmptyPredicate : TypedInstanceValidPredicate, TypedInstancePredicateProvider {
+   override fun isValid(typedInstance: TypedInstance?): Boolean {
+      return when {
+         typedInstance == null -> false
+         typedInstance is TypedNull -> false
+         typedInstance.value == null -> false
+         typedInstance.value.toString().trim().isEmpty() -> false
+         else -> true
+      }
+   }
+
+   override fun provide(field: Field): TypedInstanceValidPredicate? {
+      return if (field.metadata.any { it.name.fullyQualifiedName == "FirstNotEmpty" }) {
+         this
+      } else {
+         null
+      }
+   }
+}
