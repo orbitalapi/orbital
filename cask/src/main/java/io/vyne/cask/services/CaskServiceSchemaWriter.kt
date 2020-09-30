193296173fb72fcee422f90464f72b61109b4f46
Serhat Tuncay <serhat.tuncay@vyne.co>
2020-09-30 12:30:26 +0000

    release 0.10.0 updates

--- cask/src/main/java/io/vyne/cask/services/CaskServiceSchemaWriter.kt
+++ cask/src/main/java/io/vyne/cask/services/CaskServiceSchemaWriter.kt
@@ -8,33 +8,42 @@ import lang.taxi.generators.SchemaWriter
 import lang.taxi.types.ArrayType
 import lang.taxi.types.PrimitiveType
 import org.springframework.stereotype.Component
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
 @Component
-class CaskServiceSchemaWriter(private val schemaPublisher: SchemaPublisher, private val schemaWriter: SchemaWriter = SchemaWriter()) {
+class CaskServiceSchemaWriter(
+   private val schemaPublisher: SchemaPublisher,
+   private val schemaWriter: SchemaWriter = SchemaWriter(),
+   private val caskDefinitionPublicationExecutor: ExecutorService = Executors.newFixedThreadPool(1)) {
    private val generationCounter: AtomicInteger = AtomicInteger(0)
+   private val versionedSourceMap: MutableMap<String, VersionedSource> = mutableMapOf()
 
    fun write(taxiDocumentsByName: Map<String, TaxiDocument>) {
       // The rationale for not putting the types version ask the version for the cask schema is that
       // the cask schema generation logic will evolve independently of the underlying type that it's generated from.
-      val schemaVersion = "1.0.${generationCounter.incrementAndGet()}"
-      val schemas = taxiDocumentsByName.flatMap { (schemaName, taxiDocument) ->
-         schemaWriter.generateSchemas(listOf(taxiDocument)).mapIndexed { index, generatedSchema ->
-            val serviceSchemaWithImports = addRequiredImportsStatements(taxiDocument, generatedSchema)
-            val versionedSourceName = if (index > 0) schemaName + index else schemaName
-            VersionedSource(versionedSourceName, schemaVersion, serviceSchemaWithImports)
+      caskDefinitionPublicationExecutor.submit {
+         val schemaVersion = "1.0.${generationCounter.incrementAndGet()}"
+         val schemas = taxiDocumentsByName.flatMap { (schemaName, taxiDocument) ->
+            schemaWriter.generateSchemas(listOf(taxiDocument)).mapIndexed { index, generatedSchema ->
+               val serviceSchemaWithImports = addRequiredImportsStatements(taxiDocument, generatedSchema)
+               val versionedSourceName = if (index > 0) schemaName + index else schemaName
+               VersionedSource(versionedSourceName, schemaVersion, serviceSchemaWithImports)
+               val versionedSource = VersionedSource(versionedSourceName, schemaVersion, serviceSchemaWithImports)
+               versionedSourceMap[versionedSourceName] = versionedSource
+               versionedSource
+            }
          }
 
+         log().info("Injecting cask service schema (version=${schemaVersion}): \n${schemas.map { it.content }.joinToString(separator = "\n")}")
+
+         try {
+            schemaPublisher.submitSchemas(versionedSourceMap.values.toList())
+         } catch (e: Exception) {
+            log().error("Error in submitting schema", e)
+         }
       }
-
-      log().info("Injecting cask service schema (version=${schemaVersion}): \n${schemas.map { it.content }.joinToString(separator = "\n")}")
-
-      try {
-         schemaPublisher.submitSchemas(schemas)
-      } catch (e: Exception) {
-         log().error("Error in submitting schema", e)
-      }
-
    }
 
    // TODO This should be part of schemaWriter.
