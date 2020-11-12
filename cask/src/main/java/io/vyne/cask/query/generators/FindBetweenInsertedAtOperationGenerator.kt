af4ab18424c146b05c03e65f95156155079fd08a
Serhat Tuncay <serhat.tuncay@vyne.co>
2020-11-12 15:07:07 +0300

    Added additional between operations for queries against CaskInsertedAt

--- cask/src/main/java/io/vyne/cask/query/generators/FindBetweenInsertedAtOperationGenerator.kt
+++ cask/src/main/java/io/vyne/cask/query/generators/FindBetweenInsertedAtOperationGenerator.kt
@@ -9,11 +9,16 @@ import lang.taxi.services.OperationContract
 import lang.taxi.types.Annotation
 import lang.taxi.types.AttributePath
 import lang.taxi.types.CompilationUnit
+import lang.taxi.types.Field
+import lang.taxi.types.QualifiedName
 import lang.taxi.types.Type
 import org.springframework.stereotype.Component
 
 @Component
 class FindBetweenInsertedAtOperationGenerator(private val defaultCaskTypeProvider: DefaultCaskTypeProvider): DefaultOperationGenerator {
+
+   protected val expectedAnnotationName = OperationAnnotation.Between
+
    override fun generate(type: Type): Operation {
       val insertedAtType = defaultCaskTypeProvider.insertedAtType()
       val startParameter = TemporalFieldUtils.parameterFor(
@@ -24,11 +29,10 @@ class FindBetweenInsertedAtOperationGenerator(private val defaultCaskTypeProvide
          insertedAtType,
          TemporalFieldUtils.End,
          listOf(Annotation("PathVariable", mapOf("name" to TemporalFieldUtils.End))))
-      val greaterThanEqualConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.GREATER_THAN_OR_EQUAL_TO, TemporalFieldUtils.Start)
-      val lessThanEqualConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.LESS_THAN, TemporalFieldUtils.End)
+      val (greaterThanEqualConstraint, lessThanConstraint) = constraints(insertedAtType)
       val returnType = TemporalFieldUtils.collectionTypeOf(type)
       return Operation(
-         name = "findBy$fieldName$expectedAnnotationName",
+         name = operationName(),
          parameters = listOf(startParameter, endParameter),
          annotations = listOf(Annotation("HttpOperation", mapOf("method" to "GET", "url" to
             getRestPath(type)))),
@@ -36,18 +40,67 @@ class FindBetweenInsertedAtOperationGenerator(private val defaultCaskTypeProvide
          compilationUnits = listOf(CompilationUnit.unspecified()),
          contract = OperationContract(
             returnType = returnType,
-            returnTypeConstraints = listOf(greaterThanEqualConstraint, lessThanEqualConstraint))
+            returnTypeConstraints = listOf(greaterThanEqualConstraint, lessThanConstraint))
       )
+   }
+
+   protected fun restPath(typeQualifiedName: QualifiedName, path: AttributePath) =
+      "${CaskServiceSchemaGenerator.CaskApiRootPath}${path.parts.joinToString("/")}/${fieldName}/$expectedAnnotationName/{start}/{end}"
+
+   protected fun operationName() =  "findBy$fieldName$expectedAnnotationName"
+
+   protected fun constraints(insertedAtType: Type): BetweenConstraints {
+      return BetweenConstraints(
+         startConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.GREATER_THAN_OR_EQUAL_TO, TemporalFieldUtils.Start),
+         endConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.LESS_THAN, TemporalFieldUtils.End))
+   }
+
+   private fun getRestPath(type: Type): String {
+      val typeQualifiedName = type.toQualifiedName()
+      val path = AttributePath.from(typeQualifiedName.toString())
+      return restPath(typeQualifiedName, path)
    }
 
    companion object {
       const val fieldName = "CaskInsertedAt"
-      private val expectedAnnotationName = OperationAnnotation.Between
-
-      private fun getRestPath(type: Type): String {
-         val typeQualifiedName = type.toQualifiedName()
-         val path = AttributePath.from(typeQualifiedName.toString())
-         return "${CaskServiceSchemaGenerator.CaskApiRootPath}${path.parts.joinToString("/")}/$fieldName/$expectedAnnotationName/{start}/{end}"
-      }
    }
+}
+
+@Component
+class InsertedAtGreaterThanStartLessThanEndOperationGenerator(defaultCaskTypeProvider: DefaultCaskTypeProvider):
+   FindBetweenInsertedAtOperationGenerator(defaultCaskTypeProvider) {
+   override fun constraints(insertedAtType: Type): BetweenConstraints {
+      return BetweenConstraints(
+         startConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.GREATER_THAN, TemporalFieldUtils.Start),
+         endConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.LESS_THAN, TemporalFieldUtils.End))
+   }
+   override fun operationName() = "${super.operationName()}${BetweenVariant.GtLt}"
+   override fun restPath(typeQualifiedName: QualifiedName, path: AttributePath): String =
+      "${CaskServiceSchemaGenerator.CaskApiRootPath}${path.parts.joinToString("/")}/$fieldName/$expectedAnnotationName${BetweenVariant.GtLt}/{start}/{end}"
+}
+
+@Component
+class InsertedAtGreaterThanStartLessThanOrEqualsToEndOperationGenerator(defaultCaskTypeProvider: DefaultCaskTypeProvider):
+   FindBetweenInsertedAtOperationGenerator(defaultCaskTypeProvider) {
+   override fun constraints(insertedAtType: Type): BetweenConstraints {
+      return BetweenConstraints(
+         startConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.GREATER_THAN, TemporalFieldUtils.Start),
+         endConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.LESS_THAN_OR_EQUAL_TO, TemporalFieldUtils.End))
+   }
+   override fun operationName() = "${super.operationName()}${BetweenVariant.GtLte}"
+   override fun restPath(typeQualifiedName: QualifiedName, path: AttributePath): String =
+      "${CaskServiceSchemaGenerator.CaskApiRootPath}${path.parts.joinToString("/")}/$fieldName/$expectedAnnotationName${BetweenVariant.GtLte}/{start}/{end}"
+}
+
+@Component
+class InsertedAtGreaterThanOrEqualsToStartLessThanOrEqualsToEndOperationGenerator(defaultCaskTypeProvider: DefaultCaskTypeProvider):
+   FindBetweenInsertedAtOperationGenerator(defaultCaskTypeProvider) {
+   override fun constraints(insertedAtType: Type): BetweenConstraints {
+      return BetweenConstraints(
+         startConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.GREATER_THAN_OR_EQUAL_TO, TemporalFieldUtils.Start),
+         endConstraint = TemporalFieldUtils.constraintFor(insertedAtType, Operator.LESS_THAN_OR_EQUAL_TO, TemporalFieldUtils.End))
+   }
+   override fun operationName() = "${super.operationName()}${BetweenVariant.GteLte}"
+   override fun restPath(typeQualifiedName: QualifiedName, path: AttributePath): String =
+      "${CaskServiceSchemaGenerator.CaskApiRootPath}${path.parts.joinToString("/")}/$fieldName/$expectedAnnotationName${BetweenVariant.GteLte}/{start}/{end}"
 }
