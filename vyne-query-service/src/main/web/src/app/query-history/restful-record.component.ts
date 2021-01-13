93d20bff48443740c800ef10778c7176293bfade
Marty Pitt <martypitt@gmail.com>
2021-01-13 11:22:38 +0000

    fix overflow of labels in query history

--- vyne-query-service/src/main/web/src/app/query-history/restful-record.component.ts
+++ vyne-query-service/src/main/web/src/app/query-history/restful-record.component.ts
@@ -1,16 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {RestfulQueryHistoryRecord, RestfulQueryHistorySummary} from '../services/query.service';
+import {Component, Input} from '@angular/core';
+import {RestfulQueryHistorySummary} from '../services/query.service';
 
 @Component({
   selector: 'app-restful-record',
   template: `
     <div class="query-description">
-      <div class="given-line">
-        Given (<span class="type-name"
+      <div class="given-line clipped">
+        Given (<span class="type-name "
                      *ngFor="let factName of factTypeNames; last as isLast">{{ factName }}<span
         *ngIf="!isLast">, </span> </span>)
       </div>
-      <div class="discover-line">
+      <div class="discover-line clipped">
         <span class="verb">{{ historyRecord.query.queryMode.toLowerCase() }} </span>
         <span class="type-name">{{ expressionTypeName }}</span>
       </div>
