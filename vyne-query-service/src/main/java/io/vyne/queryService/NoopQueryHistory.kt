105a6e899c9673a3d5398dc6290e8321aea9b314
Serhat Tuncay <serhat.tuncay@vyne.co>
2021-02-14 16:20:31 +0300

    Update on history record creation mechanism

--- vyne-query-service/src/main/java/io/vyne/queryService/NoopQueryHistory.kt
+++ vyne-query-service/src/main/java/io/vyne/queryService/NoopQueryHistory.kt
@@ -1,5 +1,6 @@
 package io.vyne.queryService
 
+import io.vyne.query.QueryResponse
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
@@ -9,7 +10,7 @@ import io.vyne.query.history.QueryHistoryRecord
 @Component
 @ConditionalOnExpression("!\${vyne.query-history.enabled:true}")
 class NoopQueryHistory : QueryHistory {
-   override fun add(record: QueryHistoryRecord<out Any>) {}
+   override fun add(recordProvider: () -> QueryHistoryRecord<out Any>) {}
    override fun list(): Flux<QueryHistoryRecord<out Any>> = Flux.empty()
    override fun get(id: String): Mono<QueryHistoryRecord<out Any>> = Mono.empty()
    override fun clear() {}
