276ba107c5697638b398b352607d094c6951c126
Anthony Cowan <anthonycowan@Anthonys-Mac-Pro.local>
2021-06-23 14:23:38 +0100

    Fixed pipelines for reactive eureka

--- pipelines/pipelines-orchestrator/src/main/java/io/vyne/pipelines/orchestrator/events/PipelineEventsController.kt
+++ pipelines/pipelines-orchestrator/src/main/java/io/vyne/pipelines/orchestrator/events/PipelineEventsController.kt
@@ -3,11 +3,13 @@ package io.vyne.pipelines.orchestrator.events
 import io.vyne.utils.log
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 class PipelineEventsService : PipelineEventsApi {
-   override fun publish(@RequestBody event: PipelineEvent) {
+   override fun publish(@RequestBody event: PipelineEvent): Mono<Void> {
       log().info("$event")
+      return Mono.empty()
    }
 
 }
