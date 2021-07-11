bdf7841def7e10816f0287e64c31c09075db1a04
Marty Pitt <martypitt@gmail.com>
2021-07-11 09:46:11 +0100

    Added addition benchmark test for RemoteCallResponse

--- vyne-query-service/src/test/java/io/vyne/queryService/history/db/HistoryPersistenceQueueTest.kt
+++ vyne-query-service/src/test/java/io/vyne/queryService/history/db/HistoryPersistenceQueueTest.kt
@@ -13,6 +13,7 @@ import java.nio.file.Paths
 import java.util.*
 
 private val logger = KotlinLogging.logger {}
+
 class HistoryPersistenceQueueTest {
    @Rule
    @JvmField
@@ -74,32 +75,37 @@ class HistoryPersistenceQueueTest {
    @Test
    fun `can read remote large call response`() {
       val largeString = (0 until 100000).joinToString(separator = "") { "1" }
-      sendAndReceive(RemoteCallResponse(
-         "response-id",
-         "remote-call-id",
-         "query-id",
-         largeString
-      ))
+      sendAndReceive(
+         RemoteCallResponse(
+            "response-id",
+            "remote-call-id",
+            "query-id",
+            largeString
+         )
+      )
    }
 
    @Test
    fun `can read remote call response`() {
-      sendAndReceive(RemoteCallResponse(
+      val response = RemoteCallResponse(
          "response-id",
          "remote-call-id",
          "query-id",
          """{"icapOrderId":"2020102801000000000000007754710F_1","entryType":"FILL","orderDateTime":1603871617.912185000,"orderDate":1603843200000,"venueOrderStatus":"FILL","cfiCode":"JFTXFP","identifierValue":"EZ91WZD7WC90","tempCfiCode":"JFTXFP","tempCfiCodeForIdentType1Char":"J","tempCfiCodeForIdentType2Char":"JF","tempCfiCodeForIdentType3Char":"JFT","tempCfiCodeForIdentTypeMid":"X","identifierType":"ICAPCCYPAIR","isin":"EZ91WZD7WC90","subSecurityType":"FWD.JPY.USD.1D","priceAmount":104.2,"stopPrice":null,"priceType":"BAPO","requestedQuantity":37000000000,"cumulativeQuantity":37000000000,"remainingQuantity":0,"displayedQuantity":0,"quantityNotation":"MONE","quantityCurrency":"JPY","unitMultiplier":1,"orderType":"Limit","buySellIndicator":"BUYI","orderValidityPeriod":"IOCV","exchange":"XOFF","sourceSystem":"XOFF","tempPayReceive":"JFTXFP-BUYI","leg1PayReceive":null,"leg2PayReceive":null,"tempLegRate":"JFTXFP-BUYI","leg1Rate":null,"leg2Rate":null,"trader":null,"cacibTraderBrokerLogin":null,"brokerVenue":"XOFF","underlyingIdentifierType":"ISIN","underlyingIdentifierValue":null,"tempLegs":"JFTXFP","leg1NotionalValue":null,"leg1OrigCurrNotionalAmount":null,"leg2NotionalValue":null,"leg2OrigCurrNotionalAmount":null,"leg2Currency":null,"method":"GUI","activityCategory":"Hedge","clientid":"SC0000041353","counterpartyLei":"21380076S228I25PD704","counterParty":"ICAP EUROPE LIMITED","cacibLei":"1VUV7VQFKUOQSJ21A208","tempTradeActivityType":"Central Limit Order Book","tradeActivityType":"OTH","brokerName":"icap","caskmessageid":"b79b7aa7-16a4-43e1-ba99-5e6759618691","cask_raw_id":"834e691f-fd77-4780-bf83-57868204eb2d"} """
-      ))
+      )
+      sendAndReceive(response)
    }
 
    @Test
    fun `can read remote call response with empty response`() {
-      sendAndReceive(RemoteCallResponse(
-         "response-id",
-         "remote-call-id",
-         "query-id",
-         ""
-      ))
+      sendAndReceive(
+         RemoteCallResponse(
+            "response-id",
+            "remote-call-id",
+            "query-id",
+            ""
+         )
+      )
    }
 
    @Test
@@ -121,19 +127,21 @@ class HistoryPersistenceQueueTest {
    }
 
    @Test
-   fun benchmark() {
+   fun benchmarkQueryResultRow() {
       // NOte: I've changed the size of the large string to ~1kb, since we emit
       // 5k of these.  Using 100k (as we do in other tests) created 500mb per benchmarh run!
       val largeString = (0 until 1000).joinToString(separator = "") { "1" }
       Benchmark.benchmark("Sending result rows", warmup = 10, iterations = 30) {
          val queue = HistoryPersistenceQueue(UUID.randomUUID().toString(), tempDir.root.toPath())
          repeat((0 until 5000).count()) {
-            queue.storeResultRow(   QueryResultRow(
-               it.toLong(),
-               "query-123",
-               largeString,
-               -100293949
-            ))
+            queue.storeResultRow(
+               QueryResultRow(
+                  it.toLong(),
+                  "query-123",
+                  largeString,
+                  -100293949
+               )
+            )
          }
          logger.info { "Completed sending" }
          queue.retrieveNewResultRows()
@@ -145,8 +153,39 @@ class HistoryPersistenceQueueTest {
       }
    }
 
+   @Test
+   fun benchmarkRemoteCallResponse() {
+      // NOte: I've changed the size of the large string to ~1kb, since we emit
+      // 5k of these.  Using 100k (as we do in other tests) created 500mb per benchmarh run!
+      val largeString = (0 until 1000).joinToString(separator = "") { "1" }
+      Benchmark.benchmark("Sending result rows", warmup = 10, iterations = 30) {
+         val queue = HistoryPersistenceQueue(UUID.randomUUID().toString(), tempDir.root.toPath())
+         repeat((0 until 5000).count()) {
+            queue.storeRemoteCallResponse(
+               RemoteCallResponse(
+                  "responseId-$it",
+                  "remoteCall-$it",
+                  "queryId-$it",
+                  largeString
+               )
+            )
+         }
+         logger.info { "Completed sending" }
+         queue.retrieveNewRemoteCalls()
+            .test()
+            .expectNextCount(5000)
+            .thenCancel()
 
-   private fun sendAndReceive(response: RemoteCallResponse, queryId: String = UUID.randomUUID().toString(),  shutdownAfterCompleted:Boolean = true): HistoryPersistenceQueue {
+         queue.shutDown()
+      }
+   }
+
+
+   private fun sendAndReceive(
+      response: RemoteCallResponse,
+      queryId: String = UUID.randomUUID().toString(),
+      shutdownAfterCompleted: Boolean = true
+   ): HistoryPersistenceQueue {
       val queue = HistoryPersistenceQueue(queryId, tempDir.root.toPath())
 
       queue.retrieveNewRemoteCalls()
@@ -162,7 +201,12 @@ class HistoryPersistenceQueueTest {
       }
       return queue
    }
-   private fun sendAndReceive(resultRow: QueryResultRow, queryId: String = UUID.randomUUID().toString(), shutdownAfterCompleted:Boolean = true): HistoryPersistenceQueue {
+
+   private fun sendAndReceive(
+      resultRow: QueryResultRow,
+      queryId: String = UUID.randomUUID().toString(),
+      shutdownAfterCompleted: Boolean = true
+   ): HistoryPersistenceQueue {
       val queue = HistoryPersistenceQueue(queryId, tempDir.root.toPath())
       queue.retrieveNewResultRows()
          .test()
