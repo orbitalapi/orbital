a129656f646608ed8a9b719804b888aec50199a9
Marty Pitt <martypitt@gmail.com>
2021-08-02 11:32:29 +0100

    add new serializer to support writing TypedInstance to/from byte array

---
+++ vyne-query-service/src/test/java/io/vyne/queryService/query/SerializableTypedInstanceTest.kt
@@ -0,0 +1,62 @@
+package io.vyne.queryService.query
+
+import com.jayway.jsonpath.internal.path.PathCompiler.fail
+import io.vyne.models.SerializableTypedInstance
+import io.vyne.models.json.parseJson
+import io.vyne.models.toSerializable
+import io.vyne.testVyne
+import io.vyne.utils.Benchmark
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import org.junit.Test
+
+class SerializableTypedInstanceTest {
+   @Test
+   fun `can convert type named instance to and from bytes`() {
+      val (vyne, _) = testVyne("""
+         model Person {
+            firstName : FirstName inherits String
+            lastName : LastName inherits String
+            fullName : FullName inherits String by concat(this.firstName, ' ', this.lastName)
+         }
+         model Author inherits Person
+         model Book {
+            author : Author
+            reviewers : Person[]
+            title : BookTitle inherits String
+            subtitle: SubTitle inherits String
+            releaseDate : ReleaseDate inherits Date
+            imdbScore : ImdbScore inherits Decimal
+            released : IsReleased inherits Boolean
+         }
+      """.trimIndent())
+      val instance = vyne.parseJson("Book", """
+         {
+            "author" : {
+               "firstName" : "Jimmy",
+               "lastName" : "Schmitts"
+            },
+            "reviewers" : [ { "firstName" : "Jack" , "lastName" : "Spratt" } ],
+            "title" : "Impressing people by drinking water",
+            "subtitle" : null,
+            "releaseDate" : "2020-11-13",
+            "released" : true,
+            "imdbScore" : 3.8
+         }
+      """.trimIndent())
+      val typeNamedInstance = instance.toSerializable()
+      val bytes = Cbor.encodeToByteArray(typeNamedInstance)
+      val decoded = Cbor.decodeFromByteArray<SerializableTypedInstance>(bytes)
+      val decodedTypedInstance = decoded.toTypedInstance(vyne.schema)
+
+      Benchmark.benchmark("Serializing to/from SerializableTypedInstance", warmup = 20, iterations = 5000) {
+         val typeNamedInstance = instance.toSerializable()
+         val bytes = Cbor.encodeToByteArray(typeNamedInstance)
+         val decoded = Cbor.decodeFromByteArray<SerializableTypedInstance>(bytes)
+         val decodedTypedInstance = decoded.toTypedInstance(vyne.schema)
+         decodedTypedInstance
+      }
+      fail("TODO:  assert deserialized matched serialzied.")
+   }
+}
