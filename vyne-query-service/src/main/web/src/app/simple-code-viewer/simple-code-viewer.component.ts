1d7056973dd85bf67be49a3105feb83abd917039
Marty Pitt <martypitt@gmail.com>
2021-08-24 21:12:46 +0100

    fix scrollbars not visible in type viewer, replace code viewer with one that sizes properly, fix column width in UI resizing incorrectly

--- vyne-query-service/src/main/web/src/app/simple-code-viewer/simple-code-viewer.component.ts
+++ vyne-query-service/src/main/web/src/app/simple-code-viewer/simple-code-viewer.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import {HighlightJS} from 'ngx-highlightjs';
+import {taxiLangDef} from '../code-viewer/taxi-lang-def';
 
 @Component({
   selector: 'app-simple-code-viewer',
@@ -11,14 +13,19 @@ import {Component, Input} from '@angular/core';
         <div class="spacer"></div>
         <i class="material-icons" [ngClass]="expandWrapperClass">expand_less</i>
       </div>
-      <div class="expand-wrapper highlight-container" [ngClass]="expandWrapperClass" >
-        <td-highlight [lang]="lang" [content]="_content" *ngIf="hasContent">
-        </td-highlight>
+      <div class="expand-wrapper hljs" [ngClass]="expandWrapperClass">
+        <pre><code [highlight]="_content" [languages]="[lang]"></code></pre>
+        <!--        <td-highlight [lang]="lang" [content]="_content" *ngIf="hasContent">-->
+        <!--        </td-highlight>-->
       </div>
     </div>
   `
 })
 export class SimpleCodeViewerComponent {
+
+  constructor(private higlightJs: HighlightJS) {
+    higlightJs.registerLanguage('taxi', taxiLangDef as any);
+  }
 
   _content: string;
 
