289442b2aee17032270a5bcb7bfb740efa247375
Anthony Cowan <anthonycowan@Anthonys-Mac-Pro.local>
2021-09-01 12:11:17 +0100

    Removed global scope from cask and made dispatcher for queries configurable

--- cask/src/main/java/io/vyne/cask/config/QueryOptions.kt
+++ cask/src/main/java/io/vyne/cask/config/QueryOptions.kt
@@ -32,3 +32,20 @@ data class CaskQueryOptions(
 
    val queryMatchesNoneBehaviour: QueryMatchesNoneBehaviour = QueryMatchesNoneBehaviour.THROW_404
 )
+
+/**
+ * General principles here:
+ *  - Use enums that are self-describing versus a series of true/false flags
+ *  - Populate with reasonable defaults
+ */
+@ConstructorBinding
+@ConfigurationProperties(prefix = "cask.dispatcher")
+data class CaskQueryDispatcherConfiguration(
+   /**
+    * Size of query dispatcher thread pool size - ultimately limits the number of concurrent queries to
+    * postgres
+    */
+   val queryDispatcherPoolSize: Int = 20,
+
+
+)
