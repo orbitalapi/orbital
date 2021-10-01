ccd133c17c7dcdf2c53b1e8f82583e140123ee66
Anthony Cowan <Anthony Cowan@anthony.com>
2021-10-01 07:54:41 +0100

    Added kafka config bean

--- vyne-query-service/src/main/java/io/vyne/queryService/connectors/kafka/KafkaConnectionConfig.kt
+++ vyne-query-service/src/main/java/io/vyne/queryService/connectors/kafka/KafkaConnectionConfig.kt
@@ -1,6 +1,8 @@
 package io.vyne.queryService.connectors.kafka
 
 import io.vyne.connectors.kafka.KafkaInvoker
+import io.vyne.connectors.kafka.registry.KafkaConfigFileConnectorRegistry
+import io.vyne.connectors.kafka.registry.KafkaConnectionRegistry
 import io.vyne.queryService.connectors.jdbc.VyneConnectionsConfig
 import io.vyne.schemaStore.SchemaProvider
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -12,10 +14,17 @@ import org.springframework.context.annotation.Configuration
 class KafkaConnectionConfig {
 
    @Bean
+   fun connectionRegistry(config: VyneConnectionsConfig): KafkaConnectionRegistry {
+      return KafkaConfigFileConnectorRegistry(config.configFile)
+   }
+
+   @Bean
    fun kafkaInvoker(
+      connectionRegistry: KafkaConnectionRegistry,
       schemaProvider: SchemaProvider
    ): KafkaInvoker {
       return KafkaInvoker(
+         connectionRegistry,
          schemaProvider
       )
    }
