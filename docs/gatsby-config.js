e6819dd3c987875de2b909537de5123bd55a022f
Serhat Tuncay <serhat.tuncay@vyne.co>
2021-11-11 17:15:42 +0300

    0.18.12 and 0.19.4 release notes

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -101,6 +101,8 @@ module.exports = {
                ],
                'Release Notes': [
              // When adding release notes, newer ones at the top.
+                  'release-notes/0.19.4',
+                  'release-notes/0.18.12',
                   'release-notes/0.19.3',
                   'release-notes/0.18.11',
                   'release-notes/0.19.2',
