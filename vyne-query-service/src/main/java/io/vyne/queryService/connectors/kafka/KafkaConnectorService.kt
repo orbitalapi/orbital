958a8004b93657ba341021228f9f7a524d58c189
Marty Pitt <martypitt@gmail.com>
2022-02-28 20:06:44 +0000

    fix(connectors): fix successful connection tests showing as errors in the UI

--- vyne-query-service/src/main/java/io/vyne/queryService/connectors/kafka/KafkaConnectorService.kt
+++ vyne-query-service/src/main/java/io/vyne/queryService/connectors/kafka/KafkaConnectorService.kt
@@ -6,6 +6,7 @@ import io.vyne.connectors.kafka.KafkaConnection
 import io.vyne.connectors.kafka.KafkaConnectionConfiguration
 import io.vyne.connectors.kafka.registry.KafkaConnectionRegistry
 import io.vyne.connectors.registry.ConnectorConfigurationSummary
+import io.vyne.queryService.connectors.ConnectionTestedSuccessfully
 import io.vyne.queryService.connectors.jdbc.BadConnectionException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -20,12 +21,12 @@ class KafkaConnectorService(
 
 
    @PostMapping("/api/connections/message-broker", params = ["test=true"])
-   fun testConnection(@RequestBody connectionConfig: KafkaConnectionConfiguration): String {
+   fun testConnection(@RequestBody connectionConfig: KafkaConnectionConfiguration): ConnectionTestedSuccessfully {
       ConnectorUtils.assertAllParametersPresent(
          KafkaConnection.parameters, connectionConfig.connectionParameters
       )
       return KafkaConnection.test(connectionConfig)
-         .map { "Connection tested successfully" }
+         .map { ConnectionTestedSuccessfully }
          .getOrHandle { connectionFailureMessage ->
             throw BadConnectionException(connectionFailureMessage)
          }
