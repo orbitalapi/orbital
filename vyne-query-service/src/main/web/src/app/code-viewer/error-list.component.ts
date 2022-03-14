98b809a9857dfa3bcb954ed3b98b7b4f66e70e00
Marty Pitt <martypitt@gmail.com>
2022-03-14 18:27:10 +0000

    fix(ui/schema-explorer): fix problem error pane not visible.
    LENS-668

--- vyne-query-service/src/main/web/src/app/code-viewer/error-list.component.ts
+++ vyne-query-service/src/main/web/src/app/code-viewer/error-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {CompilationMessage} from '../services/schema';
 
 @Component({
@@ -8,17 +8,32 @@ import {CompilationMessage} from '../services/schema';
     <div class="header">
       Problems
     </div>
-    <div *ngFor="let error of errors">
-      <div class="row">
-        <span class="errorMessage">{{ error.detailMessage }}</span>
-        <span class="position">[{{error.line}},{{error.char}}]</span>
-      </div>
-    </div>
-  `
+    <table>
+      <tr *ngFor="let error of errors">
+        <td class="severity-cell" [ngClass]="error.severity.toLowerCase()">
+          <img [attr.src]="getSeverityIcon(error.severity)" class="filter-error-light">
+        </td>
+        <td class="description-cell">{{ error.detailMessage }}</td>
+        <td class="position-cell">[{{error.line}},{{error.char}}]</td>
+      </tr>
+    </table>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ErrorListComponent {
 
   @Input()
   errors: CompilationMessage[];
 
+  getSeverityIcon(severity: "INFO" | "WARNING" | "ERROR"): string {
+    const path = `assets/img/tabler`;
+    switch (severity) {
+      case "ERROR":
+        return `${path}/circle-x.svg`;
+      case "WARNING":
+        return `${path}/alert-triangle.svg`;
+      case "INFO":
+        return `${path}/info-circle.svg`
+    }
+  }
 }
--- vyne-query-service/src/main/web/src/assets/img/tabler/_README.md
+++ vyne-query-service/src/main/web/src/assets/img/tabler/_README.md
@@ -1,3 +1,4 @@
 ## Tabler Icons
 These icons are used from the Tabler icon set, which is the preferred icon set moving forwards.
 
+Browse: https://tabler-icons.io/
