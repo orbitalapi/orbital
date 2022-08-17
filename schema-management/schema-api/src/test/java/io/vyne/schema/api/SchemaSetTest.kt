346a750da2a9ae5cb24b4c8d4eb92fa953a7ee35
Marty Pitt <martypitt@gmail.com>
2022-08-17 16:01:34 +0100

    tests

--- schema-management/schema-api/src/test/java/io/vyne/schema/api/SchemaSetTest.kt
+++ schema-management/schema-api/src/test/java/io/vyne/schema/api/SchemaSetTest.kt
@@ -3,6 +3,7 @@ package io.vyne.schema.api
 import com.winterbe.expekt.should
 import io.vyne.PackageIdentifier
 import io.vyne.PackageMetadata
+import io.vyne.ParsedPackage
 import io.vyne.SourcePackage
 import org.junit.Assert.*
 import org.junit.Test
@@ -34,22 +35,23 @@ class SchemaSetTest {
 
    @Test
    fun `can add shcema with different id`() {
-      val schemaSet = SchemaSet.EMPTY
-      val packages = schemaSet.getPackagesAfterUpdate(
-         SourcePackage(
-            packageMetadata = PackageMetadata.from("com.acme", "films", "1.0.0"),
-            emptyList()
-         )
+      val schemaSet = SchemaSet.fromParsed(
+         listOf(
+            ParsedPackage(
+               PackageMetadata.from("com.acme", "films", "1.0.0"),
+               emptyList()
+            )
+         ),
+         1
       )
 
-      packages.shouldContainExactly(PackageIdentifier("com.acme", "films", "1.0.0"))
-
-      schemaSet.getPackagesAfterUpdate(
+      val result = schemaSet.getPackagesAfterUpdate(
          SourcePackage(
             packageMetadata = PackageMetadata.from("com.acme", "actors", "2.0.0"),
             emptyList()
          )
-      ).shouldContainExactly(
+      )
+      result.shouldContainExactly(
          PackageIdentifier("com.acme", "films", "1.0.0"),
          PackageIdentifier("com.acme", "actors", "2.0.0"),
       )
@@ -57,8 +59,18 @@ class SchemaSetTest {
 
    @Test
    fun `can remove schema`() {
-      TODO()
+      val schemaSet = SchemaSet.fromParsed(
+         listOf(
+            ParsedPackage(PackageMetadata.from("com.acme", "films", "1.0.0"), emptyList()),
+            ParsedPackage(PackageMetadata.from("com.acme", "actors", "2.0.0"), emptyList())
+         ),
+         1
+      )
 
+      val result = schemaSet.getPackagesAfterUpdate(null, listOf(PackageIdentifier("com.acme", "actors", "2.0.0")))
+      result.shouldContainExactly(
+         PackageIdentifier("com.acme", "films", "1.0.0"),
+      )
 
    }
 
