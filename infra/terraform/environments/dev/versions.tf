50e17a2f9ee6d5e45c117ae430666ecea2c08bce
Roope Hakulinen <roope.hakulinen@gmail.com>
2022-08-21 09:29:33 +0300

    build(deps): revert the Terraform version update due to GitLab CI/CD lagging behind

--- infra/terraform/environments/dev/versions.tf
+++ infra/terraform/environments/dev/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "=1.2.7"
+  required_version = "=1.1.9" # Needs to match the one in GitLab CI..
   required_providers {
     aws = {
       source  = "hashicorp/aws"
