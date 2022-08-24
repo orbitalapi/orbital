a097e90ecb615885ada43173614967b00fc08d03
Marty Pitt <martypitt@gmail.com>
2022-08-24 17:36:12 +0100

    chore: update storybook

--- vyne-query-service/src/main/web/.storybook/main.js
+++ vyne-query-service/src/main/web/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/app/simple-badge-list/*.stories.@(js|jsx|ts|tsx)"
+    "../src/app/changelog/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
