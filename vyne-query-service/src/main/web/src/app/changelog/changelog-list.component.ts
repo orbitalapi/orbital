ffa478a899b248afa6c8e466d1b07660395df44e
Marty Pitt <martypitt@gmail.com>
2022-09-22 12:24:12 +0100

    reverting changelog UI changes

--- vyne-query-service/src/main/web/src/app/changelog/changelog-list.component.ts
+++ vyne-query-service/src/main/web/src/app/changelog/changelog-list.component.ts
@@ -6,14 +6,14 @@ import { ChangeLogDiffEntry, ChangeLogEntry } from './changelog.service';
   template: `
     <div class="changelog-entry" *ngFor="let changeLogEntry of changeLogEntries">
       <div class="changelog-header">
-        <div class="h3">
+        <h2>
           {{ changeLogEntry.timestamp | date:'longDate' }}
-        </div>
-        <div class="h4">{{ changeLogEntry.timestamp | amTimeAgo }}</div>
+        </h2>
+        <span>{{ changeLogEntry.timestamp | amTimeAgo }}</span>
+        <h3>{{ changeLogEntry.affectedPackages[0] }}</h3>
       </div>
-      <div class="h3 affected-package">{{ changeLogEntry.affectedPackages[0] }}</div>
-
       <div class="diff-list-container">
+        <h3>Changes</h3>
         <app-diff-entry *ngFor="let diff of changeLogEntry.diffs" [diff]="diff">
 
         </app-diff-entry>
