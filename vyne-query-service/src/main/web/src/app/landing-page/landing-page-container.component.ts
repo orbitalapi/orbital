659de069aa92bf892c08fd948e5adbeb7e0fea9c
Marty Pitt <martypitt@gmail.com>
2022-09-26 09:40:45 +0100

    feat(ui/landing-page): remove changelog from landing page

--- vyne-query-service/src/main/web/src/app/landing-page/landing-page-container.component.ts
+++ vyne-query-service/src/main/web/src/app/landing-page/landing-page-container.component.ts
@@ -2,28 +2,22 @@ import { Component } from '@angular/core';
 import { TypesService } from '../services/types.service';
 import { Schema } from '../services/schema';
 import { ChangeLogEntry, ChangelogService } from '../changelog/changelog.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page-container',
   template: `
-    <app-landing-page [schema]="schema"
-                      [changeLogEntries]="changelogEntries"></app-landing-page>
+    <app-landing-page [schema$]="schema$"></app-landing-page>
   `,
   styleUrls: ['./landing-page-container.component.scss']
 })
 export class LandingPageContainerComponent {
 
-  schema: Schema;
+  schema$: Observable<Schema>;
   changelogEntries: ChangeLogEntry[] = [];
 
-  constructor(typeService: TypesService, changelogService: ChangelogService) {
-    typeService.getTypes()
-      .subscribe(schema => {
-        this.schema = schema;
-        changelogService.getChangelog()
-          .subscribe(changelog => this.changelogEntries = changelog);
-      })
-
+  constructor(typeService: TypesService) {
+    this.schema$ = typeService.getTypes();
   }
 
 }
--- vyne-query-service/src/main/web/src/app/landing-page/landing-page.component.ts
+++ vyne-query-service/src/main/web/src/app/landing-page/landing-page.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { QueryHistorySummary } from '../services/query.service';
 import { Schema } from '../services/schema';
 import { ChangeLogEntry } from '../changelog/changelog.service';
+import { Observable } from 'rxjs';
 
 export interface LandingPageCardConfig {
   title: string;
@@ -20,14 +21,8 @@ export interface LandingPageCardConfig {
     <div class="container">
       <div class="content-box schema-diagram-container">
         <h3>Your services</h3>
-        <app-schema-diagram [schema]="schema" [displayedMembers]="displayedSchemaMembers"></app-schema-diagram>
+        <app-schema-diagram [schema$]="schema$" displayedMembers="services"></app-schema-diagram>
       </div>
-      <div class="content-box changelog-container">
-        <h3>Changelog</h3>
-        <app-changelog-list [changeLogEntries]="changeLogEntries"></app-changelog-list>
-      </div>
-
-
     </div>
 
 
@@ -38,55 +33,6 @@ export class LandingPageComponent {
   }
 
   @Input()
-  changeLogEntries: ChangeLogEntry[];
+  schema$: Observable<Schema>;
 
-  // Names of types / services
-  displayedSchemaMembers: string[] = [];
-
-  private _schema: Schema;
-
-  @Input()
-  get schema(): Schema {
-    return this._schema;
-  }
-
-  set schema(value: Schema) {
-    if (value === this.schema) {
-      return;
-    }
-    this._schema = value;
-    this.resetDisplayedMembers(this.schema)
-  }
-
-  dataSources: any[] = [];
-  recentQueries: QueryHistorySummary[] = [];
-
-  recentQueryCardConfig = RECENT_QUERIES;
-  dataSourcesCardConfig = DATA_SOURCES;
-  catalogCardConfig = DATA_CATALOG;
-
-  private resetDisplayedMembers(schema: Schema) {
-    this.displayedSchemaMembers = (schema) ? schema.services.map(s => s.name.fullyQualifiedName) : [];
-  }
-}
-
-export const RECENT_QUERIES: LandingPageCardConfig = {
-  title: 'Query',
-  emptyText: `Run queries to link data from across all the registered data sources.  Get started by running your first query.`,
-  emptyActionLabel: 'Create a query',
-  emptyStateImage: 'assets/img/illustrations/search-engine.svg'
-}
-
-export const DATA_SOURCES: LandingPageCardConfig = {
-  title: 'Sources',
-  emptyText: `Data sources and schemas define the places Vyne can fetch data.  Add a data source to get started.`,
-  emptyActionLabel: 'Add a data source',
-  emptyStateImage: 'assets/img/illustrations/data-settings.svg'
-}
-
-export const DATA_CATALOG: LandingPageCardConfig = {
-  title: 'Catalog',
-  emptyText: 'A one-stop searchable catalog of all your glossary items, data models, sources and APIs registered with Vyne.',
-  emptyActionLabel: 'Search the catalog',
-  emptyStateImage: 'assets/img/illustrations/catalog.svg'
 }
