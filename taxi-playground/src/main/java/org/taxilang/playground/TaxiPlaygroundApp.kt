45305fb38095d3a1eab1a073dfbfa75f5a373477
Marty Pitt <martypitt@gmail.com>
2022-10-11 11:38:11 +0100

    feature(taxi-playground): add metrics

--- taxi-playground/src/main/java/org/taxilang/playground/TaxiPlaygroundApp.kt
+++ taxi-playground/src/main/java/org/taxilang/playground/TaxiPlaygroundApp.kt
@@ -1,6 +1,11 @@
 package org.taxilang.playground
 
+import io.micrometer.cloudwatch2.CloudWatchConfig
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry
+import io.micrometer.core.instrument.Clock
+import io.micrometer.core.instrument.MeterRegistry
 import io.vyne.query.TaxiJacksonModule
+import mu.KotlinLogging
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
@@ -10,6 +15,11 @@ import org.springframework.context.annotation.Profile
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import software.amazon.awssdk.auth.credentials.ContainerCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient
+import java.time.Duration
+
 
 @SpringBootApplication
 class TaxiPlaygroundApp {
@@ -25,7 +35,72 @@ class TaxiPlaygroundApp {
 
    @Bean
    fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
+}
 
+@Configuration
+@Profile("!dev")
+class AwsConfig {
+
+   private val logger = KotlinLogging.logger {}
+
+   @Bean
+   fun currentAwsRegion(): Region {
+      val defaultRegion = Region.US_EAST_1
+      // Seems like the most reliable way of getting region
+      // is the AWS_REGION env var.
+      val envVarRegion = System.getenv("AWS_REGION")
+      return if (envVarRegion.isNullOrEmpty()) {
+         logger.warn { "Could not determine AWS_REGION env var.  Metrics will be associated with default of $defaultRegion" }
+         defaultRegion
+      } else {
+         try {
+            logger.info { "Detected region of $envVarRegion, will be used for metrics" }
+            Region.of(envVarRegion)
+         } catch (e: Exception) {
+            logger.warn(e) { "Failed to set region from env var of $envVarRegion.  Will use default of $defaultRegion" }
+            defaultRegion
+         }
+      }
+   }
+
+   @Bean
+   fun cloudWatchAsyncClient(region: Region): CloudWatchAsyncClient {
+
+      return CloudWatchAsyncClient
+         .builder()
+         .region(region)
+         .credentialsProvider(
+            ContainerCredentialsProvider.builder().build()
+         )
+         .build()
+   }
+
+   @Bean
+   fun getMeterRegistry(client: CloudWatchAsyncClient): MeterRegistry {
+      val cloudWatchConfig = setupCloudWatchConfig()
+      return CloudWatchMeterRegistry(
+         cloudWatchConfig,
+         Clock.SYSTEM,
+         client
+      )
+   }
+
+   @Bean
+   fun tags(region: Region) = MetricsTags(listOf("region", region.id()))
+
+
+   private fun setupCloudWatchConfig(): CloudWatchConfig {
+      return object : CloudWatchConfig {
+         private val configuration = mapOf(
+            "cloudwatch.namespace" to "voyagerApp",
+            "cloudwatch.step" to Duration.ofMinutes(1).toString()
+         )
+
+         override fun get(key: String): String {
+            return configuration[key]!!
+         }
+      }
+   }
 }
 
 
@@ -35,4 +110,10 @@ class WebConfiguration : WebFluxConfigurer {
    override fun addCorsMappings(registry: CorsRegistry) {
       registry.addMapping("/**").allowedMethods("*")
    }
+
+   @Bean
+   fun tags() = MetricsTags(listOf("region", "dev"))
 }
+
+
+data class MetricsTags(val tags: List<String>)
--- taxi-playground/src/main/java/org/taxilang/playground/lsp/LanguageServerWebsocketController.kt
+++ taxi-playground/src/main/java/org/taxilang/playground/lsp/LanguageServerWebsocketController.kt
@@ -1,11 +1,16 @@
 package org.taxilang.playground.lsp
 
 import com.google.common.cache.CacheBuilder
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.binder.BaseUnits
 import lang.taxi.lsp.sourceService.WorkspaceSourceServiceFactory
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketSession
 import org.taxilang.playground.WebSocketController
 import reactor.core.publisher.Mono
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Simple websocket handler, which defers messages off to an instance
@@ -16,7 +21,22 @@ class LanguageServerWebsocketController(
    maximumSize: Int = 100,
    private val sourceServiceFactory: WorkspaceSourceServiceFactory,
    private val config: LanguageServerConfig,
+   metricsRegistry: MeterRegistry
 ) : WebSocketController {
+
+   private val activeSessions = AtomicInteger(0)
+
+   init {
+      Gauge.builder("voyager.lsp.active-connections", { activeSessions })
+         .baseUnit(BaseUnits.SESSIONS)
+         .register(metricsRegistry)
+   }
+
+   private val logger = KotlinLogging.logger {}
+   private val languageServerCache = CacheBuilder
+      .newBuilder()
+      .maximumSize(maximumSize.toLong())
+      .build<WebSocketSession, WebsocketSessionLanguageServer>()
 
    override val paths: List<String> = listOf(config.path)
    override fun handle(session: WebSocketSession): Mono<Void> {
@@ -24,15 +44,24 @@ class LanguageServerWebsocketController(
       session.receive()
          .subscribe { message -> languageServer.consume(message.payloadAsText) }
       languageServerCache.put(session, languageServer)
+      logger.info { "New LSP connection started with session ${session.id}" }
+      activeSessions.incrementAndGet()
+      logSizes()
       return session.send(languageServer.messages
+         .doFinally {
+            logger.info { "LSP connection on session ${session.id} closed" }
+            activeSessions.decrementAndGet()
+            languageServerCache.invalidate(session)
+            logSizes()
+         }
          .map { message -> session.textMessage(message) })
 
    }
 
-   private val languageServerCache = CacheBuilder
-      .newBuilder()
-      .maximumSize(maximumSize.toLong())
-      .build<WebSocketSession, WebsocketSessionLanguageServer>()
+   private fun logSizes() {
+      logger.info { "Current counts: ${activeSessions.get()} sessions, cache size of ${languageServerCache.size()}" }
+   }
+
 
 }
 
