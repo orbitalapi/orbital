944aad843993cd5285a7ad9f53610d1fbb6d88c9
Marty Pitt <martypitt@gmail.com>
2022-10-18 12:14:37 +0100

    feature(orbital): added skeleton ui project

---
+++ vyne-query-service/src/main/web/src/app/orbital-shell/orbital-shell.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orbital-shell',
+  templateUrl: './orbital-shell.component.html',
+  styleUrls: ['./orbital-shell.component.scss']
+})
+export class OrbitalShellComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ vyne-query-service/src/main/web/src/app/orbital-shell/orbital-shell.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrbitalShellComponent } from './orbital-shell.component';
+
+
+@NgModule({
+  declarations: [
+    OrbitalShellComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrbitalShellModule {
+}
