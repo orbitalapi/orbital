b6aa34d7c57d2b95211ea3fc083757c30ea8341a
Marty Pitt <martypitt@gmail.com>
2022-11-17 15:51:27 +0000

    fix(core): allow null values in equals checks, returns false

--- vyne-core-types/src/main/java/io/vyne/models/functions/stdlib/ObjectFunctions.kt
+++ vyne-core-types/src/main/java/io/vyne/models/functions/stdlib/ObjectFunctions.kt
@@ -1,6 +1,7 @@
 package io.vyne.models.functions.stdlib
 
 import io.vyne.models.*
+import io.vyne.models.functions.FunctionResultCacheKey
 import io.vyne.models.functions.NamedFunctionInvoker
 import io.vyne.models.functions.NullSafeInvoker
 import io.vyne.schemas.Schema
@@ -16,21 +17,20 @@ object ObjectFunctions {
    )
 }
 
-object Equals : NullSafeInvoker() {
+object Equals : NamedFunctionInvoker {
    override val functionName: QualifiedName = lang.taxi.functions.stdlib.Equals.name
 
-   override fun doInvoke(
-       inputValues: List<TypedInstance>,
-       schema: Schema,
-       returnType: Type,
-       function: FunctionAccessor,
-       rawMessageBeingParsed: Any?,
-       thisScopeValueSupplier: EvaluationValueSupplier,
-       returnTypeFormat: FormatsAndZoneOffset?
+   override fun invoke(
+      inputValues: List<TypedInstance>,
+      schema: Schema,
+      returnType: Type,
+      function: FunctionAccessor,
+      objectFactory: EvaluationValueSupplier,
+      returnTypeFormat: FormatsAndZoneOffset?,
+      rawMessageBeingParsed: Any?,
+      resultCache: MutableMap<FunctionResultCacheKey, Any>
    ): TypedInstance {
       val areEqual = inputValues[0] == inputValues[1]
       return TypedValue.from(schema.type(PrimitiveType.BOOLEAN), areEqual, ConversionService.DEFAULT_CONVERTER, EvaluatedExpression(function.asTaxi(), inputValues))
    }
-
-
 }
