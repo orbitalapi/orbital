34e11a0e1697c9a6c7a91fa545b68d61376b1c62
Serhat Tuncay <serhat.tuncay@vyne.co>
2023-01-25 18:50:10 +0000

    Completed missing steps in e2e test

--- e2e-tests/tests/films.spec.ts
+++ e2e-tests/tests/films.spec.ts
@@ -1,8 +1,9 @@
-import { Page, test } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
 import { AddDataSourcePage } from './pages/add-data-source.page';
 import { DialogPage } from './pages/dialog.page';
 import { SchemaImporterPage } from './pages/schema-importer.page';
 import { QueryEditorPage } from './pages/query-editor.page';
+import { doPost } from './helpers/ui';
 
 const useLocalHostNames = process.env.USE_LOCAL_HOSTNAMES === 'true';
 
@@ -25,7 +26,7 @@ const dockerComposeHostnames = {
 const hostnames = useLocalHostNames ? localHostnames : dockerComposeHostnames;
 
 test.describe('Films demo', () => {
-   test('works as described in the tutorial', async ({ page }) => {
+   test('works as described in the tutorial', async ({ page, request }) => {
       const addDataSourcePage = new AddDataSourcePage(page);
       await addDataSourcePage.goto();
 
@@ -85,8 +86,12 @@ test.describe('Films demo', () => {
       const protobufSchemaImporterPage = new SchemaImporterPage(page);
       await protobufSchemaImporterPage.openMenuSection('Models');
       await protobufSchemaImporterPage.chooseMenuItem('NewFilmReleaseAnnouncement');
-      // TODO Not working ATM
-      // await protobufSchemaImporterPage.selectTypeForParameter('filmId', 'film.types.FilmId');
+      await protobufSchemaImporterPage.selectTypeForAttribute('FilmId', 'Int', 'film.types.FilmId');
+      await protobufSchemaImporterPage.createNewType(
+         'String',
+         'AnnouncementMessage',
+         'lang.taxi.String',
+         'Stringlang.taxi.StringA collection of characters.');
       await protobufSchemaImporterPage.clickButton('Save');
       await protobufSchemaImporterPage.expectNotification('The schema was updated successfully');
       await waitForSchemaUpdate(page);
@@ -102,11 +107,13 @@ test.describe('Films demo', () => {
       const kafkaConnectionDialog = new DialogPage(page);
       await kafkaConnectionDialog.fillValue('Connection name', 'kafka');
       await kafkaConnectionDialog.fillValue('Broker address', `${hostnames.kafka}:9092`);
+      const currentEpochTime = Math.round((new Date()).getTime() / 1000);
+      await kafkaConnectionDialog.fillValue('Group Id', `${currentEpochTime}`);
       await kafkaConnectionDialog.clickButton('Create');
 
       await addDataSourcePage.setKafkaTopic('releases');
       await addDataSourcePage.openDropdown('Topic offset');
-      await addDataSourcePage.selectDropdownOption('LATEST');
+      await addDataSourcePage.selectDropdownOption('EARLIEST');
       await addDataSourcePage.fillValue('Default namespace', 'io.vyne.demos.announcements');
       await addDataSourcePage.selectOnAutoComplete('Message type', 'NewFilmReleaseAnnouncement');
       await addDataSourcePage.clickButton('Create');
@@ -133,6 +140,61 @@ test.describe('Films demo', () => {
          'A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies'
       ]);
 
-   });
+      // Streaming Query that will Join data from Kafka, API and DB.
+      await addDataSourcePage.goto();
+      await queryEditorPage.goto();
+      await queryEditorPage.runQuery('stream { NewFilmReleaseAnnouncement } as {' +
+         '    news: {' +
+         '        announcement: AnnouncementMessage' +
+         '    }' +
+         '    film: {' +
+         '        name: Title' +
+         '        id : FilmId' +
+         '        description: Description' +
+         '    }' +
+         '    productionDetails: {' +
+         '        released: ReleaseYear' +
+         '    }' +
+         '    ' +
+         '    providers: StreamingProvider[]' +
+         '}[]');
 
+      await queryEditorPage.expectStreamingQueryIsRunning();
+      // Wait for the query sets up the kafka subscription, before we post the message to kafka
+      // Otherwise, the offset on the subscription will be ahead of the last published message!
+      await queryEditorPage.waitFor(5000);
+      // push some data to kafka. We push it twice, as the first never been picked up!!
+      await doPost(request,
+         `http://${hostnames.filmsApi}/kafka/newReleases/1`,
+         1,
+         'topic',
+         'releases')
+
+      await queryEditorPage.selectResultTab('Table');
+      await queryEditorPage.expectTableHeaders([
+         'news',
+         'film',
+         'productionDetails',
+         'providers'
+      ], 5000);
+      await queryEditorPage.expectTableRow(0, [
+         'View nested structures in tree mode',
+         'View nested structures in tree mode',
+         'View nested structures in tree mode',
+         'View collections in tree mode'
+      ]);
+
+      await queryEditorPage.selectResultTab('Tree');
+
+      await queryEditorPage.expectHaveText([
+         'Today, Netflix announced the reboot of yet another classic franchise',
+         'ACADEMY DINOSAUR',
+         'A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies',
+         '2006',
+         'Disney Plus',
+         '7.99',
+         'Now TV',
+         '13.99'
+      ]);
+   });
 });
