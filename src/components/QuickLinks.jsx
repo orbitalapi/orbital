8de8a30de5b42e88c388220b2c111c6b9c64a4d0
Marty Pitt <martypitt@gmail.com>
2023-02-15 22:43:06 +0000

    fix padding

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -12,7 +12,7 @@ export function QuickLinks({children}) {
 
 export function QuickLink({title, description, href, icon}) {
   return (
-    <div className="group relative rounded-xl border border-slate-200 dark:border-slate-800">
+    <div className="group relative rounded-xl border border-slate-200 dark:border-slate-200/25">
       <div
         className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]"/>
       <div className="relative overflow-hidden rounded-xl p-6">
