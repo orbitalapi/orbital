6f33b5ea4a967b3e697af1778d43ab350f056b5a
Marty Pitt <martypitt@gmail.com>
2023-02-28 17:28:28 +0000

    chore(docs): comment out links we won't implement right now

--- src/components/docs/SidebarLinks.js
+++ src/components/docs/SidebarLinks.js
@@ -20,30 +20,30 @@ export const SidebarLinks = [
          {title: 'Pushing directly to Orbital', href: '/docs/connecting-data-sources/publishing-direct-to-orbital'},
       ],
    },
-   {
-      title: 'Querying for data',
-      links: [
-         {title: 'TaxiQL tutorial', href: '/docs/sample'},
-         {title: `Querying using Orbital's API`, href: '/docs/sample'},
-         {title: `Using an SDK`, href: '/docs/sample'}
-      ],
-   },
-   {
-      title: 'Data pipelines',
-      links: [
-         {title: 'Building a pipeline', href: '/docs/sample'},
-      ],
-   },
-   {
-      title: 'Deploying Orbital',
-      links: [
-         {title: 'Production deployments', href: '/docs/sample'},
-         {title: 'Configuring Orbital', href: '/docs/deploying/configuring-orbital'},
-         {title: 'Authenticating to other services', href: '/docs/sample'},
-         {
-            title: 'Authorization within Orbital',
-            href: '/docs/sample',
-         },
+  {
+    title: 'Querying for data',
+    links: [
+      { title: 'TaxiQL tutorial', href: '/docs/sample' },
+      { title: `Querying using Orbital's API`, href: '/docs/sample' },
+      { title: `Using an SDK`, href: '/docs/sample' }
+    ]
+  },
+  // {
+  //    title: 'Data pipelines',
+  //    links: [
+  //       {title: 'Building a pipeline', href: '/docs/sample'},
+  //    ],
+  // },
+  {
+    title: 'Deploying Orbital',
+    links: [
+      { title: 'Production deployments', href: '/docs/sample' },
+      { title: 'Configuring Orbital', href: '/docs/deploying/configuring-orbital' },
+      { title: 'Authenticating to other services', href: '/docs/sample' },
+      {
+        title: 'Authorization within Orbital',
+        href: '/docs/sample'
+      },
          {
             title: 'Standalone analytics server',
             href: '/docs/sample',
@@ -57,31 +57,31 @@ export const SidebarLinks = [
    {
       title: 'Tutorials',
       links: [
-         {title: 'Integrating APIs, DBs and Kafka', href: '/docs/writing-plugins'},
-         {title: 'Composing APIs', href: '/docs/writing-plugins'},
-         {title: 'Building a Data mesh', href: '/docs/writing-plugins'},
+        { title: 'Integrating APIs, DBs and Kafka', href: '/docs/writing-plugins' }
+        // {title: 'Composing APIs', href: '/docs/writing-plugins'},
+        // {title: 'Building a Data mesh', href: '/docs/writing-plugins'},
       ],
    },
-   {
-      title: 'Background',
-      links: [
-         {title: 'Introduction to Semantic Integration', href: '/background/intro-to-semantic-integration'},
-         {title: 'Tips on building Taxonomies', href: '/background/tips-on-taxonomies'},
-      ],
-   },
-   {
-      title: 'Troubleshooting',
-      links: [
-         {title: 'Mac M1 & Docker', href: '/troubleshooting/mac-m1-and-docker'},
-      ],
-   },
-   {
-      title: 'Release notes',
-      links: [
-         {title: '0.21.x', href: '/docs/release-notes/0.21'},
-         {title: '0.20.x', href: '/docs/release-notes/0.20'},
-         {title: '0.19.x', href: '/docs/release-notes/0.19'},
-         {title: '0.18.x', href: '/docs/release-notes/0.18'},
-      ],
-   },
+  {
+    title: 'Background',
+    links: [
+      { title: 'Introduction to Semantic Integration', href: '/background/intro-to-semantic-integration' },
+      { title: 'Tips on building Taxonomies', href: '/background/tips-on-taxonomies' }
+    ]
+  },
+  // {
+  //    title: 'Troubleshooting',
+  //    links: [
+  //       {title: 'Mac M1 & Docker', href: '/troubleshooting/mac-m1-and-docker'},
+  //    ],
+  // },
+  {
+    title: 'Release notes',
+    links: [
+      { title: '0.21.x', href: '/docs/release-notes/0.21' },
+      { title: '0.20.x', href: '/docs/release-notes/0.20' },
+      { title: '0.19.x', href: '/docs/release-notes/0.19' },
+      { title: '0.18.x', href: '/docs/release-notes/0.18' }
+    ]
+  }
 ]
