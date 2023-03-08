6abab28c8aaffe0700240fff48a330c588115db8
Marty Pitt <martypitt@gmail.com>
2023-03-08 11:43:31 +0000

    fix(ui): fix broken layouts
    ORB-70

--- vyne-query-service/src/main/web/src/app/connection-manager/connection-list.component.ts
+++ vyne-query-service/src/main/web/src/app/connection-manager/connection-list.component.ts
@@ -1,29 +1,30 @@
-import {Component, Input} from '@angular/core';
-import {ConnectorSummary, DbConnectionService} from '../db-connection-editor/db-importer.service';
-import {Observable} from 'rxjs';
-import {Router} from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { ConnectorSummary, DbConnectionService } from '../db-connection-editor/db-importer.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connection-list',
   template: `
-      <app-header-component-layout title="Connections"
-                                   [description]="'Create connections to register databases and message brokers to Orbital. '">
-          <ng-container ngProjectAs="buttons">
-              <button tuiButton size="m" (click)="createNewConnection()"
-                      [appearance]="(connections | async)?.length > 0 ? 'outline' : 'primary'">Add connection</button>
-          </ng-container>
+    <app-header-component-layout title="Connections"
+                                 [description]="'Create connections to register databases and message brokers to Orbital. '">
+      <ng-container ngProjectAs="buttons">
+        <button tuiButton size="m" (click)="createNewConnection()"
+                [appearance]="(connections | async)?.length > 0 ? 'outline' : 'primary'">Add connection
+        </button>
+      </ng-container>
 
-          <div *ngIf="(connections | async)?.length > 0;">
-              <table class="connection-list">
-                  <thead>
-                  <tr>
-                      <th>Name</th>
-                      <th>Type</th>
-                      <th>Address</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr *ngFor="let connection of connections | async">
+      <div *ngIf="(connections | async)?.length > 0;" class='connection-list-container'>
+        <table class="connection-list">
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Address</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr *ngFor="let connection of connections | async">
                       <td>{{ connection.connectionName }}</td>
                       <td>{{ connection.driverName | titlecase }}</td>
                       <td>{{ connection.address }}</td>
