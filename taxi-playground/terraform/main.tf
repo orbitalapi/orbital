8917a3c6862a74a9d1b1965e191bcb2b2c26037c
Marty Pitt <martypitt@gmail.com>
2023-03-09 12:39:52 +0000

    update kotlin blog

--- taxi-playground/terraform/main.tf
+++ taxi-playground/terraform/main.tf
@@ -6,7 +6,7 @@ locals {
 
   subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
-  domain_name             = "voyager.vyne.co"
+  domain_name             = "voyager.orbitalhq.com"
   environment_domain_name = var.environment == "prod" ? local.domain_name : "${var.environment}.${local.domain_name}"
 }
 
