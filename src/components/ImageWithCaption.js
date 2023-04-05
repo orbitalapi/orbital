4e3a391acb0a09a499ba2426e093f7eb561fb932
Michael Stone <michael.stone@orbitalhq.com>
2023-04-05 12:18:02 +0100

    Fix image display in docs

--- src/components/ImageWithCaption.js
+++ src/components/ImageWithCaption.js
@@ -4,25 +4,23 @@ import Image from 'next/future/image';
 export function ImageWithCaption({ caption, wide, src, voyagerLink, addLightBackground }) {
 
   return (
-    <div className='breakout-image'>
-      <div className={clsx(
-        "relative my-[2em] first:mt-0 last:mb-0 rounded-lg rounded-lg w-full lg:w-[65vw] mx-auto",
-        { "bg-slate-800 dark:bg-transparent": addLightBackground },
-      )}>
-        <div className="p-[1rem]">
-          <Image src={src}
-            alt={caption} 
-            sizes="(max-width: 1280px) 100vw,
+    <div className={clsx(
+      "relative my-[2em] first:mt-0 last:mb-0 rounded-lg rounded-lg w-full mx-auto",
+      { "bg-slate-800 dark:bg-transparent": addLightBackground },
+    )}>
+      <div className="p-[1rem]">
+        <Image src={src}
+          alt={caption}
+          sizes="(max-width: 1280px) 100vw,
                     65vw"
-            className="rounded-md"
-         />
+          className="rounded-md"
+        />
 
-          <div className="text-center text-slate-500 mt-[1rem]">{caption}</div>
-          {voyagerLink !== undefined &&
-            <div className="text-center"><a className="text-slate-400 text-sm" target='_blank'
-              href={`https://voyager.vyne.co/s/${voyagerLink}`}>Edit this diagram on
-              voyager.vyne.co</a></div>}
-        </div>
+        <div className="text-center text-slate-500 mt-[1rem]">{caption}</div>
+        {voyagerLink !== undefined &&
+          <div className="text-center"><a className="text-slate-400 text-sm" target='_blank'
+            href={`https://voyager.vyne.co/s/${voyagerLink}`}>Edit this diagram on
+            voyager.vyne.co</a></div>}
       </div>
     </div>
   );
