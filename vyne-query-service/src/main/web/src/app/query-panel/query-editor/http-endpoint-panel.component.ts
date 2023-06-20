49abfcf53623fa5e96d7524a253be184c2acd3b6
Marty Pitt <martypitt@gmail.com>
2023-06-20 16:07:04 +0100

    feat(ui): generation of http endpoints via ui

---
+++ vyne-query-service/src/main/web/src/app/query-panel/query-editor/http-endpoint-panel.component.ts
@@ -0,0 +1,77 @@
+import {ChangeDetectorRef, Component, Inject} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {TuiDialogContext, TuiDialogService} from "@taiga-ui/core";
+import {POLYMORPHEUS_CONTEXT} from "@tinkoff/ng-polymorpheus";
+
+@Component({
+  selector: 'app-http-endpoint-panel',
+  template: `
+    <app-header-component-layout title="Publish as HTTP API">
+      <form [formGroup]="formGroup">
+        <tui-input formControlName="endpoint" [tuiTextfieldPrefix]="prefix"
+        >
+          API Endpoint
+          <input
+            tuiTextfield
+          />
+        </tui-input>
+        <tui-error
+          formControlName="endpoint"
+          [error]="[] | tuiFieldError | async"
+        ></tui-error>
+      </form>
+      <div class="row">
+        <button
+          tuiButton
+          type="button"
+          size="m"
+          appearance="outline"
+          (click)="close()"
+        >
+          Cancel
+        </button>
+        <div class="spacer"></div>
+        <button
+          tuiButton
+          type="button"
+          size="m"
+          appearance="primary"
+          [disabled]="!formGroup.valid"
+          (click)="update()"
+        >
+          Update
+        </button>
+      </div>
+    </app-header-component-layout>
+  `,
+  styleUrls: ['./http-endpoint-panel.component.scss']
+})
+export class HttpEndpointPanelComponent {
+
+  formGroup: FormGroup
+
+  prefix = `/api/q/`
+  annotationRegex = /@HttpOperation\([^)]*\)\s*/g
+
+  constructor(@Inject(TuiDialogService) private readonly dialogs: TuiDialogService,
+              @Inject(POLYMORPHEUS_CONTEXT)
+              private readonly context: TuiDialogContext<string, string>,
+              private changeRef: ChangeDetectorRef,) {
+    this.formGroup = new FormGroup({
+      endpoint: new FormControl(null, Validators.pattern('^[a-zA-Z\\-]+\$'))
+    })
+  }
+
+  close() {
+    this.context.completeWith(null);
+  }
+
+  update() {
+    const originalQuery = this.context.data;
+    const trimmed = originalQuery.replace(this.annotationRegex, '');
+    const annotation = `@HttpOperation(url = '${this.prefix}${this.formGroup.getRawValue()['endpoint']}', method = 'GET')`
+    const updated = annotation + '\n' + trimmed;
+    console.log(updated)
+    this.context.completeWith(updated);
+  }
+}
