ece50121e88970322d1f62f160819774f196995a
Marty Pitt <martypitt@gmail.com>
2023-09-13 16:31:49 +0100

    feat(convert): added convert function

--- vyne-core-types/src/main/java/com/orbitalhq/models/functions/stdlib/StdLib.kt
+++ vyne-core-types/src/main/java/com/orbitalhq/models/functions/stdlib/StdLib.kt
@@ -2,6 +2,7 @@ package com.orbitalhq.models.functions.stdlib
 
 import com.orbitalhq.models.functions.NamedFunctionInvoker
 import com.orbitalhq.models.functions.stdlib.collections.CollectionFiltering
+import com.orbitalhq.models.functions.stdlib.transform.Transformations
 
 object StdLib {
    val functions: List<NamedFunctionInvoker> = listOf(
@@ -9,7 +10,8 @@ object StdLib {
       Functional.functions,
       Collections.functions,
       CollectionFiltering.functions,
-      ObjectFunctions.functions
+      ObjectFunctions.functions,
+      Transformations.functions
    ).flatten()
 }
 
