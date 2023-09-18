231dc483301710b8ae76dd9072dc1df37b95abf5
Marty Pitt <martypitt@gmail.com>
2023-09-18 17:20:47 +0100

    feat(ui): updated avatar compoennt to use Taiga

--- vyne-query-service/src/main/web/src/app/header-bar/header-bar.module.ts
+++ vyne-query-service/src/main/web/src/app/header-bar/header-bar.module.ts
@@ -4,26 +4,28 @@ import { HeaderBarComponent } from './header-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { SearchModule } from '../search/search.module';
 import { AvatarComponent } from './avatar.component';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ConfirmationDialogModule } from 'src/app/confirmation-dialog/confirmation-dialog.module';
 import {WorkspaceSelectorModule} from "../workspace-selector/workspace-selector.module";
+import {TuiAvatarModule} from "@taiga-ui/kit";
 
 @NgModule({
-    imports: [
-        MatToolbarModule,
-        SearchModule,
-        CommonModule,
-        MatButtonModule,
-        MatIconModule,
-        MatMenuModule,
-        MatDialogModule,
-        ConfirmationDialogModule,
-        WorkspaceSelectorModule
-    ],
+  imports: [
+    MatToolbarModule,
+    SearchModule,
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule,
+    ConfirmationDialogModule,
+    WorkspaceSelectorModule,
+    TuiAvatarModule,
+  ],
   exports: [HeaderBarComponent, AvatarComponent],
   declarations: [HeaderBarComponent, AvatarComponent],
   providers: [],
