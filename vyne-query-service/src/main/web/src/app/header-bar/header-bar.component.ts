18afcca3b15d88c436f9cce8f48230602695c12c
Marty Pitt <martypitt@gmail.com>
2023-09-20 08:16:08 +0100

    feat(ui): workspace selector behind feature toggle

--- vyne-query-service/src/main/web/src/app/header-bar/header-bar.component.ts
+++ vyne-query-service/src/main/web/src/app/header-bar/header-bar.component.ts
@@ -1,31 +1,35 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {AuthService} from '../auth/auth.service';
 import {UserInfoService, VyneUser} from '../services/user-info.service';
+import {AppConfig, AppInfoService} from "../services/app-info.service";
+import {Observable} from "rxjs";
 
 @Component({
-    selector: 'app-header-bar',
-    template: `
-        <app-workspace-selector></app-workspace-selector>
-        <div class="spacer"></div>
-        <app-search-bar-container></app-search-bar-container>
-        <app-avatar *ngIf="vyneUser" [user]="vyneUser"></app-avatar>
-    `,
-    styleUrls: ['./header-bar.component.scss']
+  selector: 'app-header-bar',
+  template: `
+    <app-workspace-selector *ngIf="(appConfig$ | async)?.featureToggles.workspacesEnabled"></app-workspace-selector>
+    <div class="spacer"></div>
+    <app-search-bar-container></app-search-bar-container>
+    <app-avatar *ngIf="vyneUser" [user]="vyneUser"></app-avatar>
+  `,
+  styleUrls: ['./header-bar.component.scss']
 })
 export class HeaderBarComponent implements OnInit {
-    vyneUser: VyneUser;
+  vyneUser: VyneUser;
+  appConfig$: Observable<AppConfig>;
 
-    constructor(private userInfoService: UserInfoService) {
-    }
+  constructor(private userInfoService: UserInfoService, private appConfigService: AppInfoService) {
+    this.appConfig$ = this.appConfigService.getConfig()
+  }
 
-    @Input()
-    title: string;
+  @Input()
+  title: string;
 
-    ngOnInit(): void {
-        this.userInfoService.userInfo$.subscribe(
-            userInfo => this.vyneUser = userInfo,
-            error => console.warn('Failed to retrieve user info - user is probably not logged in')
-        );
-    }
+  ngOnInit(): void {
+    this.userInfoService.userInfo$.subscribe(
+      userInfo => this.vyneUser = userInfo,
+      error => console.warn('Failed to retrieve user info - user is probably not logged in')
+    );
+  }
 
 }
