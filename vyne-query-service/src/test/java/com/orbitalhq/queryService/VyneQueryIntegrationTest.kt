e3a9e1f1df4e0352c82f998fb5c55975f1e17f10
Marty Pitt <martypitt@gmail.com>
2023-10-16 12:19:45 +0100

    fix(tests): fixed null references in test code

--- vyne-query-service/src/test/java/com/orbitalhq/queryService/VyneQueryIntegrationTest.kt
+++ vyne-query-service/src/test/java/com/orbitalhq/queryService/VyneQueryIntegrationTest.kt
@@ -246,7 +246,7 @@ str2|2"""
 
       response.statusCodeValue.should.be.equal(200)
       response.headers["Content-Type"].should.equal(listOf("text/event-stream;charset=UTF-8"))
-      response.body.withoutWhitespace().should.equal(
+      response.body!!.withoutWhitespace().should.equal(
          """
             data:{"userId":"1010","userName":"jean-pierre"}
             data:{"userId":"2020","userName":"jean-paul"}
@@ -387,7 +387,7 @@ str2|2"""
       response.statusCodeValue.should.be.equal(200)
       response.headers["Content-Type"].should.equal(listOf("text/csv;charset=UTF-8"))
       assertEquals(
-         response.body.trimIndent(), """
+         response.body!!.trimIndent(), """
          userId,userName
          1010,jean-pierre
          2020,jean-paul
@@ -433,7 +433,7 @@ str2|2"""
          str1|1
          str2|2
          """.trimIndent(),
-         response.body.trimIndent()
+         response.body!!.trimIndent()
       )
    }
 
