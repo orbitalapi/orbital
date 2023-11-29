1392320d7b37e56a9c7833a4df531bb67696a44c
Marty Pitt <martypitt@gmail.com>
2023-11-29 16:55:01 +0000

    update default services.conf

--- vyne-core-types/src/main/java/com/orbitalhq/http/ServicesConfig.kt
+++ vyne-core-types/src/main/java/com/orbitalhq/http/ServicesConfig.kt
@@ -10,14 +10,14 @@ data class ServicesConfig(
       val ORBITAL_SERVER_NAME = "orbital-server"
       val STREAM_SERVER_NAME = "stream-server"
       val ANALYTICS_SERVER_NAME = "analytics-server"
-      val METRICS_SERVER_NAME = "orbital-metrics-server"
+      val METRICS_SERVER_NAME = "orbital-prometheus"
 
       val DEFAULT_QUERY_SERVER_RSOCKET_PORT = 7655
 
       val DEFAULT = ServicesConfig(
          mapOf(
             ORBITAL_SERVER_NAME to mapOf(
-               "url" to "http://orbital",
+               "url" to "http://orbital:9022",
                "rsocket" to "tcp://orbital:$DEFAULT_QUERY_SERVER_RSOCKET_PORT"
             ),
             STREAM_SERVER_NAME to mapOf("url" to "http://orbital-stream-server"),
@@ -25,7 +25,7 @@ data class ServicesConfig(
                "url" to "http://orbital-query-analytics",
                "rsocket" to "tcp://vyne-analytics-server:7654"
             ),
-            "orbital-metrics-server" to mapOf("url" to "http://orbital-metrics")
+            METRICS_SERVER_NAME to mapOf("url" to "http://prometheus:9090")
          )
       )
    }
