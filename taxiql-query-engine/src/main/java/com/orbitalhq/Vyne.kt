099dfe0faaf6e016e0bf712f12fbbcb69956e3ec
Marty Pitt <martypitt@gmail.com>
2023-12-27 12:59:23 +0000

    refactor: move TaxiQL Query expression creation into a new class.

--- taxiql-query-engine/src/main/java/com/orbitalhq/Vyne.kt
+++ taxiql-query-engine/src/main/java/com/orbitalhq/Vyne.kt
@@ -175,54 +175,9 @@ class Vyne(
          queryOptions = queryOptions
       )
          .responseType(deriveResponseType(taxiQl))
-//      queryContext = taxiQl.projectedType?.let {
-//         queryContext.projectResultsTo(it, taxiQl.projectionScope) // Merge conflict, was : it.toVyneQualifiedName()
-//      } ?: queryContext
 
-      val constraintProvider = TaxiConstraintConverter(this.schema)
-      val queryExpressions = taxiQl.typesToFind.map { discoveryType ->
-
-         val targetType = when {
-            discoveryType.anonymousType != null -> ProjectionAnonymousTypeProvider.toVyneAnonymousType(discoveryType.anonymousType!!, schema)
-            StreamType.isStreamTypeName(discoveryType.typeName) && UnionType.isUnionType(discoveryType.type.typeParameters()[0]) -> {
-               val unionType = ProjectionAnonymousTypeProvider.toVyneStreamOfAnonymousType(discoveryType.type, schema)
-               unionType
-            }
-            else -> schema.type(discoveryType.typeName.toVyneQualifiedName())
-         }
-         val expression = if (discoveryType.constraints.isNotEmpty()) {
-            val constraints = constraintProvider.buildOutputConstraints(targetType, discoveryType.constraints)
-            ConstrainedTypeNameQueryExpression(targetType.name.parameterizedName, constraints)
-         } else {
-            TypeQueryExpression(targetType)
-         }
-
-         expression
-      }
-
-      val expression: QueryExpression = when {
-         queryExpressions.size > 1 -> {
-            val streamJoin =  (queryExpressions.all { it is TypeQueryExpression && it.type.isStream })
-            require(streamJoin) { "Multiple source types are only supported when joining streams" }
-            StreamJoiningExpression(queryExpressions as List<TypeQueryExpression>)
-               .applyProjection(taxiQl.projectedType, taxiQl.projectionScope, schema)
-         }
-         queryExpressions.size == 1 -> queryExpressions.first().let { expression ->
-            expression.applyProjection(taxiQl.projectedType, taxiQl.projectionScope, schema)
-         }
-
-         else -> null
-      }.let { possibleQueryExpression ->
-         // At this point we have either:
-         // Mutation -only query.
-         // Query-only query.
-         // Query-then-mutate query.
-         // Decorate encapsulates those and returns the correct expression
-         MutatingQueryExpression.decorate(possibleQueryExpression, taxiQl.mutation)
-      }
-
-
-
+      val expressionBuilder = QueryExpressionBuilder()
+      val expression = expressionBuilder.build(taxiQl, this.schema)
       return Pair(queryContext, expression)
    }
 
--- taxiql-query-engine/src/test/java/com/orbitalhq/VyneStreamMergingTest.kt
+++ taxiql-query-engine/src/test/java/com/orbitalhq/VyneStreamMergingTest.kt
@@ -7,6 +7,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flowOf
+import kotlin.time.Duration
 
 /**
  * These are tests that explore merging two streams together
@@ -50,6 +51,50 @@ class VyneStreamMergingTest : DescribeSpec({
             .results
 
          results.test {
+            tweetFlow.emit(vyne.parseJson("Tweet", """{ "messageId" : "a" , "message" : "Hello" }"""))
+            val first = expectTypedObject()
+            first.toRawObject().shouldBe(
+               mapOf(
+                  "id" to "a",
+                  "body" to "Hello",
+                  "views" to null
+               )
+            )
+
+            analyticsFlow.emit(vyne.parseJson("TweetAnalytics", """{ "messageId" : "a" , "views" : 100 }"""))
+            val second = expectTypedObject()
+            // Because we don't have anything to join the state, we should be getting nulls on values
+            // that arrived on previous messages
+            second.toRawObject().shouldBe(
+               mapOf(
+                  "id" to "a",
+                  "body" to null,
+                  "views" to 100
+               )
+            )
+
+
+         }
+      }
+
+      it("should run a query that joins multiple streams without explicit streams") {
+         val tweetFlow = MutableSharedFlow<TypedInstance>()
+         val analyticsFlow = MutableSharedFlow<TypedInstance>()
+         stub.addResponseFlow("tweets") { _, _ -> tweetFlow }
+         stub.addResponseFlow("analytics") { _, _ -> analyticsFlow }
+
+         val results = vyne.query(
+            """stream { Tweet }
+           | as {
+           |   id : MessageId
+           |   body : Message
+           |   views : ViewCount? // Comes from TweetAnalytics
+           |}[]
+        """.trimMargin()
+         )
+            .results
+
+         results.test(timeout = Duration.parse("10m")) {
             tweetFlow.emit(vyne.parseJson("Tweet", """{ "messageId" : "a" , "message" : "Hello" }"""))
             val first = expectTypedObject()
             first.toRawObject().shouldBe(
