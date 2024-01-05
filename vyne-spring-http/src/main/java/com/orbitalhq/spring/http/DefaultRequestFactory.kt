98ad9fa6b0a004234d0379176ead56ca42c734c0
Marty Pitt <martypitt@gmail.com>
2024-01-05 14:14:28 +0000

    feat(core/http): http headers supported as parameters to operation

--- vyne-spring-http/src/main/java/com/orbitalhq/spring/http/DefaultRequestFactory.kt
+++ vyne-spring-http/src/main/java/com/orbitalhq/spring/http/DefaultRequestFactory.kt
@@ -1,12 +1,15 @@
 package com.orbitalhq.spring.http
 
+import com.google.common.collect.ArrayListMultimap
+import com.google.common.collect.ListMultimap
+import com.google.common.collect.Multimaps
 import com.orbitalhq.models.TypedInstance
 import com.orbitalhq.schemas.RemoteOperation
-import lang.taxi.annotations.HttpOperation
+import lang.taxi.annotations.HttpHeader
 import lang.taxi.annotations.HttpRequestBody
+import mu.KotlinLogging
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
 import org.springframework.util.MultiValueMap
 
 interface HttpRequestFactory {
@@ -15,24 +18,45 @@ interface HttpRequestFactory {
 }
 
 class DefaultRequestFactory : HttpRequestFactory {
+   companion object {
+      private val logger = KotlinLogging.logger {}
+
+   }
+
    override fun buildRequestBody(operation: RemoteOperation, parameters: List<TypedInstance>): HttpEntity<*> {
-      if (operation.hasMetadata(HttpOperation.NAME)) {
-         // TODO Revisit as this is a quick hack to invoke services that returns simple/text
-         val httpOperation = operation.metadata(HttpOperation.NAME)
-         httpOperation.params["consumes"]?.let {
-            val httpHeaders = HttpHeaders()
-            httpHeaders.accept = mutableListOf(MediaType.parseMediaType(it as String))
-            return HttpEntity<String>(httpHeaders)
-         }
-      }
+      val headers = buildHttpHeaders(operation, parameters)
       val requestBodyParamIdx = operation.parameters.indexOfFirst { it.hasMetadata(HttpRequestBody.NAME) }
-      if (requestBodyParamIdx == -1) return HttpEntity.EMPTY
+      if (requestBodyParamIdx == -1) return HttpEntity<Any>(headers)
       // TODO : For now, only looking up param based on type.  This is obviously naieve, and should
       // be improved, using name / position?  (note that parameters don't appear to be ordered in the list).
 
       val requestBodyParamType = operation.parameters[requestBodyParamIdx].type
       val requestBodyTypedInstance = parameters.first { it.type.name == requestBodyParamType.name }
-      return HttpEntity(requestBodyTypedInstance.toRawObject())
+      return HttpEntity(requestBodyTypedInstance.toRawObject(), headers)
 
+   }
+
+   private fun buildHttpHeaders(operation: RemoteOperation, parameters: List<TypedInstance>): HttpHeaders {
+      val headers = ArrayListMultimap.create<String, String>()
+      operation.allMetadata(HttpHeader.NAME).map { HttpHeader.fromMap(it.params) }.forEach { header ->
+         headers.put(header.name, header.asValue())
+      }
+      operation.parameters.forEachIndexed {  index, parameter ->
+         if (parameter.hasMetadata(HttpHeader.NAME)) {
+            val annotation = HttpHeader.fromMap(parameter.firstMetadata(HttpHeader.NAME).params)
+            val typedInstance = parameters[index]
+            if (!typedInstance.type.isAssignableTo(parameter.type)) {
+               logger.error { "Failed to build headers for operation ${operation.name} - Parameter at index $index was expected to be type ${parameter.type.name.shortDisplayName} but was ${typedInstance.type.name.shortDisplayName}" }
+            }
+            val headerValue = typedInstance.value?.toString() ?: ""
+            headers.put(annotation.name, annotation.asValue(headerValue))
+         }
+      }
+
+      val httpHeaders = HttpHeaders()
+      headers.asMap().forEach {  (headerName, value) ->
+         httpHeaders[headerName] = value.toList()
+      }
+      return httpHeaders
    }
 }
---
+++ vyne-spring/src/test/java/com/orbitalhq/spring/invokers/HttpHeadersTest.kt
@@ -0,0 +1,152 @@
+package com.orbitalhq.spring.invokers
+
+import com.orbitalhq.Vyne
+import com.orbitalhq.http.MockWebServerRule
+import com.orbitalhq.rawObjects
+import com.orbitalhq.schema.api.SimpleSchemaProvider
+import com.orbitalhq.schemas.taxi.TaxiSchema
+import com.orbitalhq.testVyne
+import io.kotest.common.runBlocking
+import io.kotest.matchers.shouldBe
+import org.junit.Rule
+import org.junit.Test
+import org.springframework.web.reactive.function.client.WebClient
+
+class HttpHeadersTest {
+
+   @Rule
+   @JvmField
+   val server = MockWebServerRule()
+
+   private fun vyneWithHttpInvoker(src: String): Vyne {
+      return testVyne(src) { schema ->
+         listOf(
+            RestTemplateInvoker(
+               SimpleSchemaProvider(schema),
+               WebClient.create()
+            )
+         )
+      }
+   }
+
+   @Test
+   fun `http headers are passed`(): Unit = runBlocking {
+      val vyne = vyneWithHttpInvoker(
+         """
+         model Person {
+            name : Name inherits String
+         }
+         type CacheMaxAge inherits Int
+         service PeopleService {
+            @HttpOperation(method = "GET", url = "http://localhost:${server.port}/people")
+            operation findPeople(
+               @HttpHeader(name = "Cache-Control", prefix = "max-age=") cacheMaxAge : CacheMaxAge
+            ):Person[]
+         }
+      """.trimIndent()
+      )
+      prepareServerResponse()
+      vyne.query(
+         """
+         given { CacheMaxAge = 9600 }
+         find { Person[] } """
+      )
+         .rawObjects()
+      val lastRequest = server.takeRequest()
+      lastRequest.getHeader("Cache-Control").shouldBe("max-age=9600")
+   }
+
+   @Test
+   fun `can mix and match operation headers and param headers`(): Unit = runBlocking {
+      val vyne = vyneWithHttpInvoker(
+         """
+         model Person {
+            name : Name inherits String
+         }
+         type CacheMaxAge inherits Int
+         service PeopleService {
+            @HttpOperation(method = "GET", url = "http://localhost:${server.port}/people")
+            @HttpHeader(name = "Consumes", value = "application/json")
+            @HttpHeader(name = "Accept-Encoding", value = "gzip")
+            operation findPeople(
+               @HttpHeader(name = "Cache-Control", prefix = "max-age=") cacheMaxAge : CacheMaxAge
+            ):Person[]
+         }
+      """.trimIndent()
+      )
+      prepareServerResponse()
+      vyne.query(
+         """
+         given { CacheMaxAge = 9600 }
+         find { Person[] } """
+      )
+         .rawObjects()
+      val lastRequest = server.takeRequest()
+      lastRequest.getHeader("Cache-Control").shouldBe("max-age=9600")
+      lastRequest.getHeader("Consumes").shouldBe("application/json")
+      lastRequest.getHeader("Accept-Encoding").shouldBe("gzip")
+   }
+
+   private fun prepareServerResponse() {
+      server.addJsonResponse("""[ { "name" : "Jimmy" } ]""")
+   }
+
+   @Test
+   fun `can provide some paramaters headers via expressions`(): Unit = runBlocking {
+      // This test exercises where we end up doing a graph search.
+      // It's a different code path from where we call DirectServiceInvocation (tested below).
+      val vyne = vyneWithHttpInvoker(
+         """
+         model Person {
+            name : Name inherits String
+         }
+         type CacheMaxAge inherits Int
+         service PeopleService {
+            @HttpOperation(method = "GET", url = "http://localhost:${server.port}/people")
+            operation findPeople(
+               @HttpHeader(name = "Cache-Control", prefix = "max-age=") cacheMaxAge : CacheMaxAge,
+               @HttpHeader(name = "Accept-Encoding") encoding : String = upperCase("gzip")
+            ):Person[]
+         }
+      """.trimIndent()
+      )
+      prepareServerResponse()
+      vyne.query(
+         """
+         given { CacheMaxAge = 9600 }
+         find { Person[] } """
+      )
+         .rawObjects()
+      val lastRequest = server.takeRequest()
+      lastRequest.getHeader("Cache-Control").shouldBe("max-age=9600")
+      lastRequest.getHeader("Accept-Encoding").shouldBe("GZIP")
+   }
+
+
+   @Test
+   fun `can provide default http headers when all values come from expressions`(): Unit = runBlocking {
+      // This test examines the situation going through DirectServiceOperationInvoation,
+      // which doesn't use a graph search
+      val src = """
+         model Person {
+            name : Name inherits String
+         }
+         service PeopleService {
+            @HttpOperation(method = "GET", url = "http://localhost:${server.port}/people")
+            operation findPeople(
+               @HttpHeader(name = "Accept-Encoding") encoding : String = upperCase("gzip")
+            ):Person[]
+         }
+      """
+      val vyne = vyneWithHttpInvoker(src)
+      prepareServerResponse()
+      vyne.query(
+         """
+            given { s : String = "" }
+            find { Person[] } """.trimIndent()
+      )
+         .rawObjects()
+      val lastRequest = server.takeRequest()
+      lastRequest.getHeader("Accept-Encoding").shouldBe("GZIP")
+   }
+}
