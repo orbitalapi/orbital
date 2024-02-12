c7b189a8dc7ab189a81b05fd578d4764181f530c
Jason Langdon <jason.langdon@orbitalhq.com>
2024-02-12 15:41:19 +0000

    fix: Fix windows path when adding data source

--- utils/src/main/java/com/orbitalhq/utils/Path.kt
+++ utils/src/main/java/com/orbitalhq/utils/Path.kt
@@ -1,8 +1,10 @@
 package com.orbitalhq.utils
 
 import java.io.File
+import java.net.URI
 import java.net.URL
 import java.nio.file.Path
+import java.nio.file.Paths
 
 
 fun String.toPath(): Path {
@@ -10,3 +12,14 @@ fun String.toPath(): Path {
 }
 
 fun String.asResource(): URL = Thread.currentThread().contextClassLoader.getResource(this)
+
+fun resolvePossiblyAbsolutePath(pathString: String, relativeTo: Path): Path {
+   return try {
+      val path = Paths.get(URI.create(pathString))
+      if (!path.isAbsolute) {
+         relativeTo.resolve(pathString)
+      } else path
+   } catch (e:Exception) {
+      relativeTo.resolve(pathString)
+   }
+}
