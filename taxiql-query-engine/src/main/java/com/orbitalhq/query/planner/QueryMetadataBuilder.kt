d003364db79718bf1b025385a0cd4b0986864cfd
Marty Pitt <martypitt@gmail.com>
2024-03-08 04:13:11 +0000

    chore: re-enable cache

--- taxiql-query-engine/src/main/java/com/orbitalhq/query/planner/QueryMetadataBuilder.kt
+++ taxiql-query-engine/src/main/java/com/orbitalhq/query/planner/QueryMetadataBuilder.kt
@@ -31,7 +31,6 @@ class QueryMetadataBuilder(cacheSize: Int = 50) {
     */
    fun buildMetadata(taxiQlQuery: TaxiQlQuery, schema: Schema): QueryPlanMetadata {
       val cacheKey = QueryPlanCacheKey(taxiQlQuery, schema)
-      return doBuildMetadata(taxiQlQuery, schema)
       return cache.get(cacheKey.hashCode()) {
          doBuildMetadata(taxiQlQuery, schema)
       }
