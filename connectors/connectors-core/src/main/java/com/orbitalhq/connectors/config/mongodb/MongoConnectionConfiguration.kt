348acdcce31bbcc8e478ff1cbdee4386006f95e2
Serhat Tuncay <serhat@sigun.com.tr>
2024-04-02 12:00:21 +0300

    wire up mongo

--- connectors/connectors-core/src/main/java/com/orbitalhq/connectors/config/mongodb/MongoConnectionConfiguration.kt
+++ connectors/connectors-core/src/main/java/com/orbitalhq/connectors/config/mongodb/MongoConnectionConfiguration.kt
@@ -7,7 +7,7 @@ import com.orbitalhq.utils.obfuscateKeys
 import kotlinx.serialization.Serializable
 
 @Serializable
-class MongoConnectionConfiguration( override val connectionName: String,
+data class MongoConnectionConfiguration( override val connectionName: String,
                                     val connectionParameters: Map<ConnectionParameterName, String>):
    ConnectorConfiguration {
    override val type: ConnectorType = ConnectorType.NO_SQL
