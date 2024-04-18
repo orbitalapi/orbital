9f7249c914cef7bbee5b8d4e9dd5b32fb7a65656
Jason Langdon <jason.langdon@orbitalhq.com>
2024-04-18 14:38:40 +0100

    add ellipses to avatar username

--- orbital-ui/src/app/header-bar/avatar.component.ts
+++ orbital-ui/src/app/header-bar/avatar.component.ts
@@ -12,7 +12,7 @@ import {TUI_PROMPT, TuiPromptData} from "@taiga-ui/kit";
       <div class="user-container">
         <tui-avatar [text]="user.username" [rounded]="true" [autoColor]="true" [avatarUrl]="user.profileUrl"
                     size="s"></tui-avatar>
-        <div>{{ user.name || user.username }}</div>
+        <div class="username">{{ user.name || user.username }}</div>
       </div>
     </tui-hosted-dropdown>
     <ng-template #userMenu>
