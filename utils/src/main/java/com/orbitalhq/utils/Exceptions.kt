fd569831395b795c9c6f1fb7a223d7c69d41b29b
Marty Pitt <martypitt@gmail.com>
2024-05-15 14:09:01 +0100

    perf(graph-query-strategy): don't fill in the stack trace of exceptions thrown during searches, as it's expensive and on the hot path.

--- utils/src/main/java/com/orbitalhq/utils/Exceptions.kt
+++ utils/src/main/java/com/orbitalhq/utils/Exceptions.kt
@@ -9,3 +9,19 @@ fun throwUnrecoverable(e: Exception) {
       -> throw e
    }
 }
+
+
+/**
+ * A performance optimized exception that doesn't provide stack information.
+ * Use this when you want to throw an exception to signal a failed process, but
+ * don't need to capture stack trace (ie., the exception is caught upstream, or
+ * the failure is expected).
+ *
+ * Filling in the stack is expensive.
+ */
+abstract class NoStackException(message:String) : RuntimeException(message) {
+   override fun fillInStackTrace(): Throwable {
+      // Don't fill in the stack trace
+      return this
+   }
+}
