74b66b0d96a5b5444ab4fb47ccf93bea1738009d
Serhat Tuncay <serhat.tuncay@vyne.co>
2024-06-27 15:06:20 +0000

    ORB-439. AWS health check
    Closes ORB-439

---
+++ cockpit-core/src/main/java/com/orbitalhq/cockpit/core/connectors/aws/AwsConnectionHealthProvider.kt
@@ -0,0 +1,30 @@
+package com.orbitalhq.cockpit.core.connectors.aws
+
+import arrow.core.getOrElse
+import com.orbitalhq.cockpit.core.connectors.ConnectionHealthProvider
+import com.orbitalhq.connections.ConnectionStatus
+import com.orbitalhq.connectors.config.aws.AwsConnectionConfiguration
+import com.orbitalhq.connectors.registry.ConnectorConfiguration
+import com.orbitalhq.connectors.registry.ConnectorType
+import io.vyne.connectors.aws.core.registry.test
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+@Component
+class AwsConnectionHealthProvider: ConnectionHealthProvider {
+    override fun canProvideFor(config: ConnectorConfiguration): Boolean  = config.type == ConnectorType.AWS
+
+    override fun provide(config: ConnectorConfiguration): Mono<ConnectionStatus> {
+        require(config is AwsConnectionConfiguration) { "Expected to receive a ${AwsConnectionConfiguration::class.simpleName}, but got ${config::class.simpleName}"}
+        return Mono.create { sink ->
+            val result = try {
+                config.test()
+                    .map { ConnectionStatus.healthy() }
+                    .getOrElse { ConnectionStatus.error(it) }
+            } catch (e:Exception) {
+                ConnectionStatus.error(e.message ?: e::class.simpleName!!)
+            }
+            sink.success(result)
+        }
+    }
+}
