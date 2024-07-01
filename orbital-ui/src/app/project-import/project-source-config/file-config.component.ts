a1e32c6a342bb3c1ab0da08a19c6c7de1a9f09d2
Marty Pitt <martypitt@gmail.com>
2024-07-01 11:12:24 +0100

    fix(ui): fix error thrown when creating new taxi project
    fix ORB-476

--- orbital-ui/src/app/project-import/project-source-config/file-config.component.ts
+++ orbital-ui/src/app/project-import/project-source-config/file-config.component.ts
@@ -18,153 +18,154 @@ import {FileRepositoryTestResponse, SchemaImporterService} from 'src/app/project
 @Component({
   selector: 'app-file-config',
   template: `
-      <div class='form-header-text'>
-          <p>Read projects directly from the local machine.</p>
-          <tui-notification status='warning'>Disk based projects are great for getting started and dev-local
-              experiments,
-              however you should consider storing your project in a git repository in production
-          </tui-notification>
-      </div>
-      <form #fileForm='ngForm'>
-          <div class='form-container'>
-              <div class='form-body'>
-                  <div class='form-row'>
-                      <div class='form-item-description-container'>
-                          <h3>Project type</h3>
-                          <div class='help-text'>
-                              Git repositories can contain full Taxi projects, or individual API specs.
-                          </div>
-                      </div>
-                      <div class='form-element'>
-                          <tui-select
-                                  [readOnly]='!editable'
-                                  [stringify]='stringifyProjectType'
-                                  [ngModel]='fileSystemPackageConfig.loader.packageType'
-                                  (ngModelChange)='selectedProjectTypeChanged($event)'
-                                  name='project-type' required
-                          >
-                              Project type
-                              <tui-data-list *tuiDataList>
-                                  <button tuiOption value='Taxi'>{{ stringifyProjectType('Taxi') }}</button>
-                                  <button tuiOption value='OpenApi'>{{ stringifyProjectType('OpenApi') }}</button>
-                                  <button tuiOption value='Avro'>{{ stringifyProjectType('Avro') }}</button>
-                              </tui-data-list>
-                          </tui-select>
-                      </div>
-                  </div>
-                  <app-open-api-package-config *ngIf="fileSystemPackageConfig.loader.packageType ==='OpenApi'"
-                                               [openApiPackageSpec]='openApiPackageSpec'
-                                               [projectType]="'file'"
-                                               [editable]="editable"
-                                               [(path)]='fileSystemPackageConfig.path'
-                                               ngModelGroup="openApiForm"
-                  ></app-open-api-package-config>
-                  <app-avro-package-config
-                          *ngIf="fileSystemPackageConfig.loader.packageType === 'Avro'"
-                          [packageSpec]="avroPackageSpec"
-                          [editable]="editable"
-                          [(path)]='fileSystemPackageConfig.path'
-                          ngModelGroup="avroForm"
-                  ></app-avro-package-config>
-                  <ng-container *ngIf="fileSystemPackageConfig.loader.packageType === 'Taxi'">
-                      <div class='form-row'>
-                          <div class='form-item-description-container'>
-                              <h3>Project path</h3>
-                              <div class='help-text'>
-                                  <p>
-                                      Specify the path to the directory containing a <code>taxi.conf</code> file.
-                                  </p>
-                              </div>
-                          </div>
-                          <div class='form-element'>
-                              <div class='row'>
-                                  <div style='flex-grow: 1;'>
-                                      <tui-input [ngModel]='fileSystemPackageConfig.path' class='flex-grow'
-                                                 name='pathToTaxi' required [readOnly]='!editable'
-                                                 (ngModelChange)='filePathUpdated($event)'>
-                                          Path
-                                      </tui-input>
-                                      <div style='display: flex; margin-top: 0.5rem'>
-                                          <tui-loader [showLoader]='true' size='s'
-                                                      *ngIf='editable && !filePathTestResult && fileSystemPackageConfig.path'
-                                                      [textContent]="'Checking for a taxi project file at ' + expectedTaxiConfLocation"
-                                          ></tui-loader>
-                                          <tui-notification *ngIf='filePathTestResult?.exists' status='success'>
-                                              Great - we've found project {{ filePathTestResult.identifier.id }} there
-                                          </tui-notification>
-                                          <div style='display: flex; width: 100%; align-items: center;'
-                                               *ngIf='filePathTestResult && !filePathTestResult.exists && !filePathTestResult.errorMessage'>
-                                              <tui-notification style='flex-grow: 1'
-                                                                status='info'>
-                                                  Can't find a project at {{ expectedTaxiConfLocation }}
-                                              </tui-notification>
-                                              <button tuiButton size='s' appearance='outline' style='margin-left: 1rem'
-                                                      (click)='createNewProject()'>Create new project
-                                              </button>
-                                          </div>
-                                          <div style='display: flex; width: 100%; align-items: center;'
-                                               *ngIf='filePathTestResult && filePathTestResult.errorMessage'>
-                                              <tui-notification style='flex-grow: 1'
-                                                                status='error'>
-                                                  {{ filePathTestResult.errorMessage }}
-                                              </tui-notification>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class='form-row' *ngIf='creatingNewProject'>
-                          <div class='form-item-description-container'>
-                              <h3>Package identifier</h3>
-                              <div class='help-text'>
-                                  All schemas in {{ UiCustomisations.productName }} need a Package Identifier - similar
-                                  to npm or maven
-                                  co-ordinates
-                              </div>
-                          </div>
-                          <div class='form-element'>
-                              <div tuiGroup>
-                                  <app-package-identifier-input
-                                          [(packageIdentifier)]="fileSystemPackageConfig.newProjectIdentifier"
-                                          [editable]="editable"></app-package-identifier-input>
-                              </div>
-                          </div>
-                      </div>
-                      <div class='form-row'>
-                          <div class='form-item-description-container'>
-                              <h3>Enable edits</h3>
-                              <div class='help-text'>
-                                  <p>
-                                      If enabled, edits can be made through the {{ UiCustomisations.productName }} UI
-                                  </p>
-                              </div>
-                          </div>
-                          <div class='form-element'>
-                              <tui-checkbox [(ngModel)]='fileSystemPackageConfig.isEditable' name='editable'
-                                            required [readOnly]='!editable'></tui-checkbox>
-                          </div>
-                      </div>
-                  </ng-container>
-              </div>
-          </div>
-      </form>
-      <div *ngIf='editable' class='form-button-bar'>
-          <button
-                  tuiButton
-                  *ngIf="isOnboardingMode"
-                  appearance="secondary"
-                  [size]="'m'"
-                  (click)="goBackOnboarding.emit()"
-          >Cancel
-          </button>
-          <button tuiButton [showLoader]='working' [size]="'m'" (click)='doCreate()' [disabled]='fileForm.invalid'>
-              Create
-          </button>
-      </div>
-      <tui-notification [status]='saveResultMessage.severity.toLowerCase()' *ngIf='saveResultMessage'>
-          {{ saveResultMessage.message }}
+    <div class='form-header-text'>
+      <p>Read projects directly from the local machine.</p>
+      <tui-notification status='warning'>Disk based projects are great for getting started and dev-local
+        experiments,
+        however you should consider storing your project in a git repository in production
       </tui-notification>
+    </div>
+    <form #fileForm='ngForm'>
+      <div class='form-container'>
+        <div class='form-body'>
+          <div class='form-row'>
+            <div class='form-item-description-container'>
+              <h3>Project type</h3>
+              <div class='help-text'>
+                Git repositories can contain full Taxi projects, or individual API specs.
+              </div>
+            </div>
+            <div class='form-element'>
+              <tui-select
+                [readOnly]='!editable'
+                [stringify]='stringifyProjectType'
+                [ngModel]='fileSystemPackageConfig.loader.packageType'
+                (ngModelChange)='selectedProjectTypeChanged($event)'
+                name='project-type' required
+              >
+                Project type
+                <tui-data-list *tuiDataList>
+                  <button tuiOption value='Taxi'>{{ stringifyProjectType('Taxi') }}</button>
+                  <button tuiOption value='OpenApi'>{{ stringifyProjectType('OpenApi') }}</button>
+                  <button tuiOption value='Avro'>{{ stringifyProjectType('Avro') }}</button>
+                </tui-data-list>
+              </tui-select>
+            </div>
+          </div>
+          <app-open-api-package-config *ngIf="fileSystemPackageConfig.loader.packageType ==='OpenApi'"
+                                       [openApiPackageSpec]='openApiPackageSpec'
+                                       [projectType]="'file'"
+                                       [editable]="editable"
+                                       [(path)]='fileSystemPackageConfig.path'
+                                       ngModelGroup="openApiForm"
+          ></app-open-api-package-config>
+          <app-avro-package-config
+            *ngIf="fileSystemPackageConfig.loader.packageType === 'Avro'"
+            [packageSpec]="avroPackageSpec"
+            [editable]="editable"
+            [(path)]='fileSystemPackageConfig.path'
+            ngModelGroup="avroForm"
+          ></app-avro-package-config>
+          <ng-container *ngIf="fileSystemPackageConfig.loader.packageType === 'Taxi'">
+            <div class='form-row'>
+              <div class='form-item-description-container'>
+                <h3>Project path</h3>
+                <div class='help-text'>
+                  <p>
+                    Specify the path to the directory containing a <code>taxi.conf</code> file.
+                  </p>
+                </div>
+              </div>
+              <div class='form-element'>
+                <div class='row'>
+                  <div style='flex-grow: 1;'>
+                    <tui-input [ngModel]='fileSystemPackageConfig.path' class='flex-grow'
+                               name='pathToTaxi' required [readOnly]='!editable'
+                               (ngModelChange)='filePathUpdated($event)'>
+                      Path
+                    </tui-input>
+                    <div style='display: flex; margin-top: 0.5rem'>
+                      <tui-loader [showLoader]='true' size='s'
+                                  *ngIf='editable && !filePathTestResult && fileSystemPackageConfig.path'
+                                  [textContent]="'Checking for a taxi project file at ' + expectedTaxiConfLocation"
+                      ></tui-loader>
+                      <tui-notification *ngIf='filePathTestResult?.exists' status='success'>
+                        Great - we've found project {{ filePathTestResult.identifier.id }} there
+                      </tui-notification>
+                      <div style='display: flex; width: 100%; align-items: center;'
+                           *ngIf='filePathTestResult && !filePathTestResult.exists && !filePathTestResult.errorMessage'>
+                        <tui-notification style='flex-grow: 1'
+                                          status='info'>
+                          Can't find a project at {{ expectedTaxiConfLocation }}
+                        </tui-notification>
+                        <button tuiButton size='s' appearance='outline' style='margin-left: 1rem'
+                                (click)='createNewProject()'>Create new project
+                        </button>
+                      </div>
+                      <div style='display: flex; width: 100%; align-items: center;'
+                           *ngIf='filePathTestResult && filePathTestResult.errorMessage'>
+                        <tui-notification style='flex-grow: 1'
+                                          status='error'>
+                          {{ filePathTestResult.errorMessage }}
+                        </tui-notification>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class='form-row' *ngIf='creatingNewProject'>
+              <div class='form-item-description-container'>
+                <h3>Package identifier</h3>
+                <div class='help-text'>
+                  All schemas in {{ UiCustomisations.productName }} need a Package Identifier - similar
+                  to npm or maven
+                  co-ordinates
+                </div>
+              </div>
+              <div class='form-element'>
+                <div tuiGroup>
+                  <app-package-identifier-input
+                    ngModelGroup="taxiCoordinates"
+                    [(packageIdentifier)]="fileSystemPackageConfig.newProjectIdentifier"
+                    [editable]="editable"></app-package-identifier-input>
+                </div>
+              </div>
+            </div>
+            <div class='form-row'>
+              <div class='form-item-description-container'>
+                <h3>Enable edits</h3>
+                <div class='help-text'>
+                  <p>
+                    If enabled, edits can be made through the {{ UiCustomisations.productName }} UI
+                  </p>
+                </div>
+              </div>
+              <div class='form-element'>
+                <tui-checkbox [(ngModel)]='fileSystemPackageConfig.isEditable' name='editable'
+                              required [readOnly]='!editable'></tui-checkbox>
+              </div>
+            </div>
+          </ng-container>
+        </div>
+      </div>
+    </form>
+    <div *ngIf='editable' class='form-button-bar'>
+      <button
+        tuiButton
+        *ngIf="isOnboardingMode"
+        appearance="secondary"
+        [size]="'m'"
+        (click)="goBackOnboarding.emit()"
+      >Cancel
+      </button>
+      <button tuiButton [showLoader]='working' [size]="'m'" (click)='doCreate()' [disabled]='fileForm.invalid'>
+        Create
+      </button>
+    </div>
+    <tui-notification [status]='saveResultMessage.severity.toLowerCase()' *ngIf='saveResultMessage'>
+      {{ saveResultMessage.message }}
+    </tui-notification>
   `,
   styleUrls: ['./file-config.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
