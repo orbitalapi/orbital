88d27935f7baf4db3560c915337f1eddd7085b2a
Serhat Tuncay <serhat@sigun.com.tr>
2024-12-24 11:15:30 +0300

    updates for orbital core type publisher

--- orbital-taxi-publisher/src/main/java/com/orbitalhq/internal/packagePublisher/BuiltInsSourcePackageWriter.kt
+++ orbital-taxi-publisher/src/main/java/com/orbitalhq/internal/packagePublisher/BuiltInsSourcePackageWriter.kt
@@ -4,7 +4,9 @@ import com.orbitalhq.cockpit.core.schemas.BuiltInTypesProvider
 import picocli.CommandLine
 import java.nio.file.FileSystems
 import java.nio.file.Paths
+import kotlin.io.path.exists
 import kotlin.io.path.readText
+import kotlin.io.path.writeLines
 import kotlin.io.path.writeText
 
 /**
@@ -12,35 +14,45 @@ import kotlin.io.path.writeText
  * to a taxi project
  */
 class BuiltInsSourcePackageWriter(
-   private val outputPath: String,
-   private val version: String,
+   private val config: OrbitalPackagePublisherConfig,
    private val commandLine: CommandLine) {
 
    private fun printOut(message: String) = commandLine.out.println(message)
 
    fun write() {
+      val gitRepo = GitTransport(config, commandLine)
       updateVersion()
       writeSources()
+      gitRepo.commitAndTryPush()
    }
 
    private fun updateVersion() {
-      val basePath = Paths.get(outputPath!!)
+      val basePath = Paths.get(config.outputPath)
       val taxiConfFile = basePath.resolve("taxi.conf")
-      printOut("Updating version in $${taxiConfFile} to $version")
-      val taxiConfSrc = taxiConfFile.readText()
-      val lines = taxiConfSrc.lines().toMutableList()
-      val versionLine = taxiConfSrc.lines()
-         .indexOfFirst { it.startsWith("version:") }
-      lines[versionLine] = "version: $version"
-      taxiConfFile.writeText(lines.joinToString("\n"))
+      printOut("Updating version in $${taxiConfFile} to ${config.version}")
+      if (taxiConfFile.exists()) {
+         val taxiConfSrc = taxiConfFile.readText()
+         val lines = taxiConfSrc.lines().toMutableList()
+         val versionLine = taxiConfSrc.lines()
+            .indexOfFirst { it.startsWith("version:") }
+         lines[versionLine] = "version: ${config.version}"
+         taxiConfFile.writeText(lines.joinToString("\n"))
+      } else {
+         taxiConfFile.toFile().parentFile.mkdirs()
+         taxiConfFile.toFile().createNewFile()
+         taxiConfFile.writeLines(listOf(
+            "name: com.orbitalhq/core",
+            "version: ${config.version}",
+            "sourceRoot: src/"
+         ))
+      }
    }
 
    private fun writeSources() {
-      val basePath = Paths.get(outputPath!!)
+      val basePath = Paths.get(config.outputPath)
       printOut("Writing sources using base path of $basePath")
       BuiltInTypesProvider.sourcePackage
          .sources.forEach { versionedSource ->
-
             val separator = FileSystems.getDefault().separator
             val filename = versionedSource.name.replace(".", separator)
             val filePath = Paths.get(basePath.toString(), "${separator}src${separator}", "${filename}.taxi")
--- orbital-taxi-publisher/src/main/java/com/orbitalhq/internal/packagePublisher/GitTransport.kt
+++ orbital-taxi-publisher/src/main/java/com/orbitalhq/internal/packagePublisher/GitTransport.kt
@@ -1,67 +1,233 @@
 package com.orbitalhq.internal.packagePublisher
 
-import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
-import picocli.CommandLine
-import java.nio.file.Path
+import org.eclipse.jgit.api.CreateBranchCommand
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.ListBranchCommand
+import org.eclipse.jgit.api.PushCommand
+import org.eclipse.jgit.transport.CredentialItem
+import org.eclipse.jgit.transport.CredentialItem.CharArrayType
+import org.eclipse.jgit.transport.CredentialItem.InformationalMessage
+import org.eclipse.jgit.transport.CredentialItem.StringType
+import org.eclipse.jgit.transport.CredentialItem.YesNoType
+import org.eclipse.jgit.transport.CredentialsProvider
+import org.eclipse.jgit.transport.RefSpec
 import org.eclipse.jgit.transport.SshTransport
-import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory
-import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig
-import org.eclipse.jgit.util.FS
+import org.eclipse.jgit.transport.URIish
+import org.eclipse.jgit.transport.sshd.SshdSessionFactory
+import picocli.CommandLine
 import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.system.exitProcess
+
 
 class GitTransport(
-   private val repoUrl: String,
-   private val workDir: Path,
-   private val sshKeyPath: String,
-   private val commandLine: CommandLine
+    private val config: OrbitalPackagePublisherConfig,
+    private val commandLine: CommandLine
 ) {
 
-   private fun printOut(message: String) = commandLine.out.println(message)
+    private fun printOut(message: String) = commandLine.out.println(message)
+    private var git: Git
 
-   fun cloneRepository(): Git {
-      printOut("Cloning repository...")
-      val sessionFactory = object : JschConfigSessionFactory() {
-         override fun configure(host: OpenSshConfig.Host, session: Session) {
-            session.setConfig("StrictHostKeyChecking", "no")
-         }
+    init {
+        val basePath = Paths.get(config.outputPath)
+        val basePathFile = basePath.toFile()
+        basePathFile.parentFile.mkdirs()
+        if (basePathFile.exists() && !basePathFile.isDirectory) {
+            printOut("${basePath.toFile()} is not directory!")
+            exitProcess(1)
+        }
+        if (!basePathFile.exists()) {
+            basePathFile.mkdir()
+        }
 
-         override fun createDefaultJSch(fs: FS?): JSch {
-            val defaultJSch = super.createDefaultJSch(fs)
-            defaultJSch.addIdentity(sshKeyPath)
-            return defaultJSch
-         }
-      }
 
-      return Git.cloneRepository()
-         .setURI(repoUrl)
-         .setDirectory(workDir.toFile())
-         .setTransportConfigCallback { transport ->
-            (transport as? SshTransport)?.sshSessionFactory = sessionFactory
-         }
-         .call()
-   }
+        if (basePathFile.listFiles().isNotEmpty()) {
+            printOut("$basePathFile is not empty!")
+            exitProcess(1)
+        }
 
-   fun commit(git:Git, tag:String?, push:Boolean = true) {
-      printOut("Committing changes...")
-      git.add().addFilepattern(".").call()
-      git.commit()
-         .setMessage("Automated update")
-         .setAllowEmpty(false)
-         .call()
+        printOut("Cloning to ${basePathFile.absolutePath}")
+        if (config.remoteRepositoryUrl != null) {
+            printOut("Cloning from remote repository ${config.remoteRepositoryUrl}")
 
-      tag?.let {
-         printOut("Adding tag: $it")
-         git.tag().setName(it).call()
-      }
+        }
+        git = cloneRepository(basePathFile)
+        checkout(git)
+    }
 
-      if (push) {
-         printOut("Pushing changes...")
-         git.push().call()
-         if (tag != null) {
-            git.push().setPushTags().call()
-         }
-      }
-   }
+
+    private fun cloneRepository(localPath: File): Git {
+        printOut("Cloning repository...")
+        printOut("Using the ssh key file => ${config.sshKeyPath}")
+        val remoteUrl = config.remoteRepositoryUrl ?: "file://${localPath.absolutePath}"
+        return Git.cloneRepository()
+            .setURI(remoteUrl)
+            .setDirectory(localPath)
+            .setCloneAllBranches(true)
+            .setTransportConfigCallback { transport ->
+                if (transport is SshTransport) {
+                    transport.credentialsProvider = CustomCredentialProvider()
+                    transport.sshSessionFactory = CustomSshFactory(config.sshKeyPath)
+                }
+            }
+            .call()
+    }
+
+    private fun checkout(git: Git) {
+        val current = currentBranch(git)
+        if (current == config.version) {
+            printOut("Already on branch ${config.version}")
+        }
+
+        val localBranchExists = localBranchExists(git, config.version)
+        //refs/remotes/origin/main
+        val remoteExists = remoteBranchExists(git, "origin", config.version)
+
+        when {
+            localBranchExists -> git.checkout().setName(config.version).call()
+            remoteExists -> git.checkout().setName(config.version).setCreateBranch(true).setForce(true)
+                .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK)
+                .setStartPoint("origin" + "/" + config.version).call()
+            else -> {
+                git.branchCreate().setName(config.version).setForce(true).call();
+                git.checkout().setName(config.version).call();
+                pushWithSsh()
+                    .setRemote("origin")
+                    .setRefSpecs(RefSpec(config.version))
+                    .setForce(true)
+                    .call()
+            }
+        }
+    }
+
+    private fun currentBranch(git: Git): String? {
+        return try {
+            git.repository.branch
+        } catch (e: IOException) {
+            null
+        }
+    }
+
+    private fun localBranchExists(git: Git, branch: String): Boolean {
+        val list = git.branchList().call()
+        val fullName = "refs/heads/$branch"
+        var exists = false
+        for (ref in list) {
+            val name = ref.name
+            if (name == fullName) {
+                exists = true
+            }
+        }
+        return exists
+    }
+
+    private fun remoteBranchExists(git: Git, remote: String, branch: String): Boolean {
+        val list = git.branchList().setListMode(ListBranchCommand.ListMode.REMOTE).call()
+        val fullName = "refs/remotes/$remote/$branch"
+        var exists = false
+        for (ref in list) {
+            val name = ref.name
+            if (name == fullName) {
+                exists = true
+            }
+        }
+        return exists
+    }
+
+    fun commitAndTryPush() {
+        printOut("Committing changes...")
+        git.add().addFilepattern(".").call()
+        git.commit()
+            .setMessage("Automated update")
+            .setAllowEmpty(true)
+            .call()
+
+        config.tagName?.let {
+            printOut("Adding tag: $it")
+            git
+                .tag()
+                .setForceUpdate(true)
+                .setName(it).call()
+        }
+
+        if (config.remoteRepositoryUrl != null) {
+            printOut("Pushing changes...")
+            git
+            pushWithSsh().call()
+            if (config.tagName != null) {
+                pushWithSsh()
+                    .setForce(true)
+                    .setPushTags().call()
+            }
+        } else {
+            printOut("--remote-url is not specified not pushing the changes.")
+        }
+
+    }
+
+    private fun pushWithSsh(): PushCommand {
+        return git.push()
+            .setTransportConfigCallback { transport ->
+                if (transport is SshTransport) {
+                    transport.credentialsProvider = CustomCredentialProvider()
+                    transport.sshSessionFactory = CustomSshFactory(config.sshKeyPath)
+                }
+            }
+
+    }
+}
+
+class CustomSshFactory(private val sshKeyFilePath: String) : SshdSessionFactory() {
+    private val tempDirectoryForSshSessionFactory = Files.createTempDirectory("ssh-temp-dir")
+
+    override fun getSshDirectory(): File {
+        return tempDirectoryForSshSessionFactory.toFile()
+    }
+
+    override fun getDefaultIdentities(sshDir: File): MutableList<Path> {
+        val keyFilePath = tempDirectoryForSshSessionFactory.resolve("keyfile")
+        Files.write(keyFilePath, File(sshKeyFilePath).readBytes())
+        return mutableListOf(keyFilePath)
+    }
+}
+
+class CustomCredentialProvider(private val passPhrase: String? = null) : CredentialsProvider() {
+    override fun isInteractive(): Boolean {
+        return false
+    }
+
+    override fun supports(vararg p0: CredentialItem?): Boolean {
+        return true
+    }
+
+    override fun get(p0: URIish?, vararg items: CredentialItem): Boolean {
+        for (item in items) {
+            if (item is InformationalMessage) {
+                continue
+            }
+
+            if (item is YesNoType) {
+                item.value = true
+            } else if (item is CharArrayType) {
+                if (passPhrase != null) {
+                    item.value = passPhrase.toCharArray()
+                } else {
+                    return false
+                }
+            } else if (item is StringType) {
+                if (passPhrase != null) {
+                    item.value = passPhrase
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
 }
