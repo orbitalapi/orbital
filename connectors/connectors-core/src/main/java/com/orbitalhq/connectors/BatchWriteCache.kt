7a7ce05f5d69d648a72831289527fb6f260037c8
Marty Pitt <martypitt@gmail.com>
2025-01-02 07:06:42 +0000

    tidied batch writer

--- connectors/connectors-core/src/main/java/com/orbitalhq/connectors/BatchWriteCache.kt
+++ connectors/connectors-core/src/main/java/com/orbitalhq/connectors/BatchWriteCache.kt
@@ -1,19 +1,9 @@
 package com.orbitalhq.connectors
 
+import com.orbitalhq.models.OperationResultReference
 import com.orbitalhq.models.TypedInstance
 import com.orbitalhq.utils.RetryFailOnSerializeEmitHandler
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.channels.ClosedReceiveChannelException
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.channels.ticker
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.selects.select
 import mu.KotlinLogging
-import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
 import reactor.core.publisher.Sinks
@@ -22,81 +12,62 @@ import java.time.Duration
 import java.util.concurrent.ConcurrentHashMap
 
 
-private val logger = KotlinLogging.logger {  }
+private val logger = KotlinLogging.logger { }
 
 class BatchWriteCacheProvider {
-    private val batchDataCache = ConcurrentHashMap<String, BatchWriteCache>()
+   private val batchDataCache = ConcurrentHashMap<String, BatchWriteCache>()
 
-    fun withBatchSize(queryId: String,
-                      batchSize: Int,
-                      batchTimeoutInMillis: Long,
-                      bulkCallBack:  (List<Pair<TypedInstance, FluxSink<TypedInstance>>>) -> Mono<Any>
-    ): BatchWriteCache {
-        return batchDataCache.getOrPut(queryId) {
-            val sink = Sinks.many().multicast().onBackpressureBuffer<Pair<TypedInstance, FluxSink<TypedInstance>>>()
-            sink
-                .asFlux()
-                .bufferTimeout(batchSize, Duration.ofMillis(batchTimeoutInMillis))
-                .doFinally {
-                    logger.info { "Removing sink for $queryId" }
-                batchDataCache.remove(queryId)
-            }.subscribeOn(Schedulers.boundedElastic())
-                .flatMap {
-                    bulkCallBack(it)
-                }
-                .subscribe()
-
-            BatchWriteCache(sink)
-        }
-    }
-
-    fun hasBatchCache(queryId: String) = batchDataCache.contains(queryId)
-}
-
-data class BatchWriteCache(private val sink: Sinks.Many<Pair<TypedInstance, FluxSink<TypedInstance>>>) {
-    fun emit(item: TypedInstance): Flux<TypedInstance> {
-        return Flux.create { fluxSink ->
-            sink.emitNext(Pair(item, fluxSink), RetryFailOnSerializeEmitHandler)
-        }
-    }
-}
-
-@OptIn(ObsoleteCoroutinesApi::class, ExperimentalCoroutinesApi::class)
-fun <T> Flow<T>.bufferTimeout(size: Int, duration: Duration): Flow<List<T>> {
-    require(size > 0) { "Window size should be greater than 0" }
-    require(duration.toMillis() > 0) { "Duration should be greater than 0" }
-
-    return flow {
-        coroutineScope {
-            val events = ArrayList<T>(size)
-            val tickerChannel = ticker(duration.toMillis())
-            try {
-                val upstreamValues = produce { collect { send(it) } }
-
-                while (isActive) {
-                    var hasTimedOut = false
-
-                    select<Unit> {
-                        upstreamValues.onReceive {
-                            events.add(it)
-                        }
-
-                        tickerChannel.onReceive {
-                            hasTimedOut = true
-                        }
-                    }
-
-                    if (events.size == size || (hasTimedOut && events.isNotEmpty())) {
-                        emit(events.toList())
-                        events.clear()
-                    }
-                }
-            } catch (e: ClosedReceiveChannelException) {
-                // drain remaining events
-                if (events.isNotEmpty()) emit(events.toList())
-            } finally {
-                tickerChannel.cancel()
+   /**
+    * Creates a batch writer for the provided query id - if not already present.
+    *
+    * The provided callback is responsible for writing the actual batch.
+    * The callback will only be invoked by one of the batch emitters - the one that created the cache entry.
+    */
+   fun forQueryId(
+      queryId: String,
+      batchSize: Int,
+      batchTimeoutInMillis: Long,
+      writeOperation: (List<TypedInstance>) -> Mono<OperationResultReference>
+   ): BatchWriteCache {
+      return batchDataCache.getOrPut(queryId) {
+         val sink = Sinks.many().multicast().onBackpressureBuffer<Pair<TypedInstance, Sinks.One<OperationResultReference>>>(1000)
+         sink.asFlux()
+            .doOnEach { next ->
+               logger.info { "Next item recevied" }
             }
-        }
-    }
+//            .delayElements(Duration.ofSeconds(5))
+            .bufferTimeout(batchSize, Duration.ofMillis(batchTimeoutInMillis))
+            .publishOn(Schedulers.boundedElastic())
+            .subscribe { batch ->
+
+               // HACK... this is me exprimenting with delays...
+//               val batch = listOf(next)
+               val instancesToWrite = batch.map { it.first }
+               val callbacks = batch.map { it.second }
+               writeOperation(instancesToWrite).subscribe { operationResult->
+                  callbacks.forEach { it.tryEmitValue(operationResult) }
+               }
+            }
+         return BatchWriteCache(sink, batchSize, batchTimeoutInMillis)
+      }
+   }
+
 }
+
+
+data class BatchWriteCache(
+   private val sink: Sinks.Many<Pair<TypedInstance, Sinks.One<OperationResultReference>>>,
+   private val batchSize: Int,
+   private val batchTimeoutInMillis: Long,
+) {
+   /**
+    * Emits the item to be persisted in the batch.
+    * Returns a Mono<> that completes when this item has been persisted.
+    */
+   fun emit(item: TypedInstance): Mono<OperationResultReference> {
+      val completionSink = Sinks.one<OperationResultReference>()
+      sink.emitNext(item to completionSink, RetryFailOnSerializeEmitHandler)
+      return completionSink.asMono()
+   }
+}
+
--- connectors/nosql-connectors/mongodb-connector/src/main/java/com/orbitalhq/connectors/nosql/mongodb/MongoBulkMutatingQueryInvoker.kt
+++ connectors/nosql-connectors/mongodb-connector/src/main/java/com/orbitalhq/connectors/nosql/mongodb/MongoBulkMutatingQueryInvoker.kt
@@ -1,5 +1,6 @@
 package com.orbitalhq.connectors.nosql.mongodb
 
+import com.mongodb.bulk.BulkWriteResult
 import com.mongodb.client.model.InsertOneModel
 import com.mongodb.client.model.UpdateOneModel
 import com.mongodb.client.model.UpdateOptions
@@ -13,108 +14,98 @@ import com.orbitalhq.schemas.RemoteOperation
 import com.orbitalhq.schemas.Service
 import com.orbitalhq.schemas.fqn
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.reactive.asFlow
 import mu.KotlinLogging
 import org.bson.Document
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
 
 private val logger = KotlinLogging.logger { }
+
 class MongoBulkMutatingQueryInvoker(
-    connectionFactory: MongoConnectionFactory,
-    schemaProvider: SchemaProvider,
-    private val batchWriteCacheProvider: BatchWriteCacheProvider
-): MongoBaseInvoker(connectionFactory, schemaProvider) {
+   connectionFactory: MongoConnectionFactory,
+   schemaProvider: SchemaProvider,
+   private val batchWriteCacheProvider: BatchWriteCacheProvider
+) : MongoBaseInvoker(connectionFactory, schemaProvider) {
 
-    suspend fun invoke(
-        service: Service,
-        operation: RemoteOperation,
-        parameters: List<Pair<Parameter, TypedInstance>>,
-        eventDispatcher: QueryContextEventDispatcher,
-        queryId: String,
-        batchAttribute: MongoConnector.Annotations.BatchAttribute
+   suspend fun invoke(
+      service: Service,
+      operation: RemoteOperation,
+      parameters: List<Pair<Parameter, TypedInstance>>,
+      eventDispatcher: QueryContextEventDispatcher,
+      queryId: String,
+      batchAttribute: MongoConnector.Annotations.BatchAttribute
 
-    ): Flow<TypedInstance> {
-        require(operation.parameters.size == 1) { "Operations annotated with ${MongoConnector.Annotations.UpsertOperationAnnotationName} should accept exactly one type" }
-        val inputType = operation.parameters.single().type.let { type -> type.collectionType ?: type }
-        require(inputType.hasMetadata(MongoConnector.Annotations.Collection.NAME.fqn()))
-        { "The input type into an ${MongoConnector.Annotations.UpsertOperationAnnotationName} operation should be a type with a ${MongoConnector.Annotations.Collection.NAME.fqn()} annotation" }
+   ): Flow<TypedInstance> {
+      require(operation.parameters.size == 1) { "Operations annotated with ${MongoConnector.Annotations.UpsertOperationAnnotationName} should accept exactly one type" }
+      val inputType = operation.parameters.single().type.let { type -> type.collectionType ?: type }
+      require(inputType.hasMetadata(MongoConnector.Annotations.Collection.NAME.fqn()))
+      { "The input type into an ${MongoConnector.Annotations.UpsertOperationAnnotationName} operation should be a type with a ${MongoConnector.Annotations.Collection.NAME.fqn()} annotation" }
 
-        val (param, input) = parameters.singleOrNull()
-            ?: error("Expected a single parameter, but received ${parameters.size}")
-        val collectionName = inputType.taxiType.collectionNameOrTypeName()
+      val (param, input) = parameters.singleOrNull()
+         ?: error("Expected a single parameter, but received ${parameters.size}")
+      val collectionName = inputType.taxiType.collectionNameOrTypeName()
 
-        val (connectionConfig, reactiveMongoTemplate) = getConnectionConfigAndTemplate(service)
-        val recordToWrite = parameters[0].second
-        val documentMap = typedInstanceToMap(recordToWrite)
+      val (connectionConfig, reactiveMongoTemplate) = getConnectionConfigAndTemplate(service)
+      val recordToWrite = parameters[0].second
 
-        val batchWriter = batchWriteCacheProvider.withBatchSize(queryId, batchAttribute.batchSize, batchAttribute.batchDurationInMillis) { items ->
-            bulkOpsCallback(reactiveMongoTemplate, collectionName, items)
-        }
-
-        return batchWriter.emit(recordToWrite)
+      val batchWriteBatcher = batchWriteCacheProvider.forQueryId(
+         queryId,
+         batchAttribute.batchSize,
+         batchAttribute.batchDurationInMillis
+      ) { items ->
+         doBulkInsert(reactiveMongoTemplate, collectionName, items)
             .elapsed()
             .map { durationAndData ->
-                val duration = durationAndData.t1
-                logger.info { "Mongo Upsert call completed in ${duration}ms "}
-                val data = durationAndData.t2
-                val operationResult = buildOperationResult(
-                    service,
-                    operation,
-                    listOf( input),
-                    "upsert",
-                    connectionConfig.connectionString.hosts.joinToString(),
-                    java.time.Duration.ofMillis(duration),
-                    recordCount = 1
-                )
-                eventDispatcher.reportRemoteOperationInvoked(operationResult, queryId)
-                DataSourceUpdater.update(data, operationResult.asOperationReferenceDataSource())
-            }.asFlow().onCompletion {
-                logger.warn { "completed......" }
+               val duration = durationAndData.t1
+               logger.info { "Mongo Upsert call completed in ${duration}ms " }
+               val operationResult = buildOperationResult(
+                  service,
+                  operation,
+                  items,
+                  "upsert",
+                  connectionConfig.connectionString.hosts.joinToString(),
+                  java.time.Duration.ofMillis(duration),
+                  recordCount = items.size,
+               )
+               eventDispatcher.reportRemoteOperationInvoked(operationResult, queryId)
+               operationResult.asOperationReferenceDataSource()
             }
-    }
+      }
+      return batchWriteBatcher.emit(recordToWrite)
+         .map { operationResult ->
+            DataSourceUpdater.update(recordToWrite, operationResult)
+         }.asFlow()
+   }
 
-    private fun bulkOpsCallback(
-        reactiveMongoTemplate: ReactiveMongoTemplate,
-        collectionName: String,
-        items: List<Pair<TypedInstance, FluxSink<TypedInstance>>>
-                                ): Mono<Any> {
-        return reactiveMongoTemplate.getCollection(collectionName).flatMap { mongoCollection ->
-            val writeModels = items.map { item ->
-                val recordToWrite = item.first
-                val documentMap = typedInstanceToMap(recordToWrite)
-                val upsertDefinition = MongoCriteriaGenerator.bulkUpsertFor(recordToWrite, documentMap)
-                if (upsertDefinition == null) {
-                    InsertOneModel(Document(documentMap))
-                } else {
-                    val documentQuery = upsertDefinition.first.queryObject
-                    val documentUpdate = upsertDefinition.second.updateObject
-                    /**
-                     * With replaceOne() you can only replace the entire document, while updateOne() allows for updating fields.
-                     */
-                    UpdateOneModel(documentQuery, documentUpdate, UpdateOptions().upsert(true))
-                }
+   private fun doBulkInsert(
+      reactiveMongoTemplate: ReactiveMongoTemplate,
+      collectionName: String,
+      items: List<TypedInstance>
+   ): Mono<BulkWriteResult> {
+      return reactiveMongoTemplate.getCollection(collectionName).flatMap { mongoCollection ->
+         val writeModels = items.map { recordToWrite ->
+            val documentMap = typedInstanceToMap(recordToWrite)
+            val upsertDefinition = MongoCriteriaGenerator.bulkUpsertFor(recordToWrite, documentMap)
+            if (upsertDefinition == null) {
+               InsertOneModel(Document(documentMap))
+            } else {
+               val documentQuery = upsertDefinition.first.queryObject
+               val documentUpdate = upsertDefinition.second.updateObject
+               /**
+                * With replaceOne() you can only replace the entire document, while updateOne() allows for updating fields.
+                */
+               UpdateOneModel(documentQuery, documentUpdate, UpdateOptions().upsert(true))
             }
+         }
 
-            Mono.from(
-                mongoCollection.bulkWrite(writeModels)
-            ).onErrorResume { t ->
-                logger.error(t) { "error performing bulk write for $collectionName" }
-                items.forEach { it.second.error(t) }
-                Mono.empty()
-            }
-                .map { bulkWriteResult ->
-                items.forEach {
-                    it.second.next(it.first)
-                    it.second.complete()
-                }
-                bulkWriteResult
-            }
-
+        Mono.from(
+            mongoCollection.bulkWrite(writeModels)
+         ).doOnError { t ->
+           logger.error(t) { "error performing bulk write for $collectionName" }
         }
-    }
+      }
+   }
 }
 
 /**
