bc3c2f94cf75deaf5f32a37d843a0ab07030e657
Marty Pitt <martypitt@gmail.com>
2025-02-26 17:28:07 +0000

    chore(stream-manager): reduce log noise
    ORB-915

--- pipelines/stream-engine/src/main/java/com/orbitalhq/pipelines/jet/streams/PersistentStreamManager.kt
+++ pipelines/stream-engine/src/main/java/com/orbitalhq/pipelines/jet/streams/PersistentStreamManager.kt
@@ -46,14 +46,11 @@ class PersistentStreamManager(
              * Consider moving this check up to the point where we fire SchemaSetChangedEvent.
              */
             if (newContentHash != oldContentHash) {
-               logger.info { "new content hash => $newContentHash" }
-               logger.info { "old content hash => $oldContentHash" }
-               logger.info { "Schema changed, now on generation ${schemaSetChangedEvent.newSchemaSet.generation} old generation is ${schemaSetChangedEvent.oldSchemaSet?.generation}- checking for changes to managed streams" }
-               logger.info { "Schema changed, now on schema hash ${schemaSetChangedEvent.newSchemaSet.schema.hash} old generation is ${schemaSetChangedEvent.oldSchemaSet?.schema?.hash}- checking for changes to managed streams" }
                if (this.pipelineManager.canStartPipelines()) {
+                  logger.info { "Schema has updated - updating streams" }
                   updateStreams(schemaSetChangedEvent.newSchemaSet.schema)
                } else {
-                  logger.warn { "Skipping the stream updates as I am not the leader!" }
+                  logger.info { "Schema has updated - however this node is not the leader - no action taken" }
                }
             } else {
                logger.warn { "Schema is updated but source content hash are the same, skipping stream updates!" }
