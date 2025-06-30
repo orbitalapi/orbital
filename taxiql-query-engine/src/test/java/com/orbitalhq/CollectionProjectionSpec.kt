3b42633884837cbf63de80d75ceb7fbfaa1b2bda
Marty Pitt <martypitt@gmail.com>
2025-06-30 12:01:03 +0100

    added placeholder tests for handling projecting null array, expecting to return null arrray.
    ORB-979

--- taxiql-query-engine/src/test/java/com/orbitalhq/CollectionProjectionSpec.kt
+++ taxiql-query-engine/src/test/java/com/orbitalhq/CollectionProjectionSpec.kt
@@ -69,6 +69,78 @@ class CollectionProjectionSpec : DescribeSpec({
             result.shouldBe(mapOf("title" to "Star Wars", "cast" to emptyList<Map<String, Any>>()))
          }
 
+         // Commented this out, as it's not implemented, and I'm not sure it should be.
+         // It makes sense on first pass (null -> null), but I need to understand the
+         // broader implications are for (null -> query/discover).
+         // ORB-979
+         xit("should return null when the source is null, and source is loaded from another lookup") {
+            val (vyne,stub) = testVyne("""
+            closed model Actor {
+               name : Name inherits String
+            }
+            closed model Film {
+               title: Title inherits String
+            }
+            closed model CastList {
+               cast : Actor[]
+            }
+            model ProjectedCast {
+               actorsName : Name
+            }
+            service FilmApi {
+               operation getFilms():Film[]
+               operation getCast():CastList
+            }
+         """.trimIndent())
+            stub.addResponse("getFilms","""
+            [{
+               "title" : "Star Wars"
+            }]
+         """.trimIndent())
+            stub.addResponse("getCast", """{ "cast": null }""")
+            val result = vyne.query("""find { Film[] } as {
+               |  title : Title
+               |  cast: CastList as Actor[] as ProjectedCast[]
+               |}[]
+            """.trimMargin())
+               .firstTypedInstace()
+            result.shouldBe(mapOf("title" to "Star Wars", "cast" to emptyList<Map<String, Any>>()))
+         }
+
+
+         // Commented this out, as it's not implemented, and I'm not sure it should be.
+         // It makes sense on first pass (null -> null), but I need to understand the
+         // broader implications are for (null -> query/discover).
+         // ORB-979
+         xit("should return null when the source is null") {
+            val (vyne,stub) = testVyne("""
+            model Actor {
+               name : Name inherits String
+            }
+            model Film {
+               title: Title inherits String
+               cast: Actor[]
+            }
+            service FilmApi {
+               operation getFilms():Film[]
+            }
+         """.trimIndent())
+            stub.addResponse("getFilms","""
+            [{
+               "title" : "Star Wars",
+               "cast" : null
+            }]
+         """.trimIndent())
+            val result = vyne.query("""find { Film[] } as {
+               |  title : Title
+               |  cast: {
+               |     actorsName : Name
+               |  }[]
+               |}[]
+            """.trimMargin())
+               .firstRawObject()
+            result.shouldBe(mapOf("title" to "Star Wars", "cast" to null))
+         }
       }
 
    }
