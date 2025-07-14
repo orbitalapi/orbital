52cba6fd47f770ee1bad9fab0ca5daa8d2c918d5
Marty Pitt <martypitt@gmail.com>
2025-07-14 06:37:49 +0100

    feat(stdlib): add emptyInstance()
    ORB-985

--- vyne-core-types/src/main/java/com/orbitalhq/models/functions/stdlib/ObjectFunctions.kt
+++ vyne-core-types/src/main/java/com/orbitalhq/models/functions/stdlib/ObjectFunctions.kt
@@ -4,6 +4,7 @@ import com.orbitalhq.models.*
 import com.orbitalhq.models.functions.FunctionResultCacheKey
 import com.orbitalhq.models.functions.NamedFunctionInvoker
 import com.orbitalhq.models.functions.NullSafeInvoker
+import com.orbitalhq.models.functions.stdlib.collections.createFailureWithTypedNull
 import com.orbitalhq.schemas.Schema
 import com.orbitalhq.schemas.Type
 import lang.taxi.functions.FunctionAccessor
@@ -13,7 +14,8 @@ import lang.taxi.types.QualifiedName
 
 object ObjectFunctions {
    val functions:List<NamedFunctionInvoker> = listOf(
-      Equals
+      Equals,
+      EmptyInstance
    )
 }
 
@@ -33,4 +35,43 @@ object Equals : NamedFunctionInvoker {
       val areEqual = inputValues[0] == inputValues[1]
       return TypedValue.from(schema.type(PrimitiveType.BOOLEAN), areEqual, ConversionService.DEFAULT_CONVERTER, EvaluatedExpression(function.asTaxi(), inputValues))
    }
+}
+
+
+object EmptyInstance : NullSafeInvoker() {
+   override fun doInvoke(
+      inputValues: List<TypedInstance>,
+      schema: Schema,
+      returnType: Type,
+      function: FunctionAccessor,
+      rawMessageBeingParsed: Any?,
+      thisScopeValueSupplier: EvaluationValueSupplier,
+      returnTypeFormat: FormatsAndZoneOffset?,
+      resultCache: MutableMap<FunctionResultCacheKey, Any>
+   ): TypedInstance {
+      val typeReference = inputValues[0]
+      if (typeReference !is TypeReferenceInstance) {
+         return createFailureWithTypedNull("Expected arg 0 to be a TypeReference, but instead got ${typeReference::class.simpleName}", returnType, function, inputValues)
+      }
+      val type = typeReference.type
+      val empty = buildEmptyMap(type, schema)
+      val dataSource = EvaluatedExpression(function.asTaxi(), inputValues)
+      val result = TypedInstance.from(type, empty, schema, source = dataSource)
+      return result
+   }
+
+   private fun buildEmptyMap(type: Type, schema: Schema): Any? {
+      return when {
+         type.isScalar -> null
+         type.isCollection -> emptyList<Any?>()
+         type.isStream -> emptyList<Any?>()
+         else -> {
+            type.attributes.map { (name,field) ->
+               name to buildEmptyMap(schema.type(field.type), schema)
+            }.toMap()
+         }
+      }
+   }
+
+   override val functionName: QualifiedName = lang.taxi.functions.stdlib.EmptyInstance.name
 }
